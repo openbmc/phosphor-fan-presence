4f6e218bd869ef3a22619a91d5520d28bfc79380
Matthew Barth <msbarth@us.ibm.com>
2017-01-20 15:31:58 -0600

    WIP: Function to get fan tachs
    A function to access the tach property on the fan dbus object accessed
    using the object mapper.
    
    Change-Id: I77016c690f7e1291f4ec0ac7e01a81d56b13bb0b
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ fan_presence_by_tach.cpp
@@ -0,0 +1,22 @@
+#include "config.h"
+#include "fan_presence_interface.hpp"
+
+int main(void)
+{
+    //TODO Read fan-presence-detect-by-tach conf file for list of fans
+    std::string fanPath = "xyz/openbmc_project/sensors/fan_tach/blower1";
+
+    // Get the tach for each fan listed
+    auto tach = phosphor::fan::presence::getTach(
+                    fanPath,
+                    FAN_SENSOR_VALUE_IFACE);
+
+    //TODO Create/update inventory with fan presence when tach > 0
+    if (tach > 0)
+    {
+        fprintf(stdout, "Fan present with tach = %llu\n", tach);
+    }
+
+    //TODO Monitor for fan presence changes when powered on(daemon?)
+    return 0;
+}
