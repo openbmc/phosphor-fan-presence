d4750d6acf055b78e33eb734dd76776eae263b57
Matthew Barth <msbarth@us.ibm.com>
2017-01-20 15:31:58 -0600

    WIP: Function to get fan tachs
    A function to access the tach property on the fan dbus object accessed
    using the object mapper.
    
    Change-Id: I77016c690f7e1291f4ec0ac7e01a81d56b13bb0b
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ fan_presence_interface.cpp
@@ -0,0 +1,59 @@
+#include <sdbusplus/bus.hpp>
+#include "fan_presence_interface.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace presence
+{
+
+    //TODO Should get these from phosphor-objmgr config.h
+    constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+    constexpr auto MAPPER_PATH = "/xyz/openbmc_project/ObjectMapper";
+    constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
+
+    //TODO Able to return `auto` here?
+    int getTach(const std::string& objpath, const std::string& iface)
+    {
+        int64_t tach;
+        std::map<std::string, std::vector<std::string>> methodResponse;
+
+        auto bus = sdbusplus::bus::new_default();
+        auto method = bus.new_method_call(
+                        MAPPER_BUSNAME,
+                        MAPPER_PATH,
+                        MAPPER_INTERFACE,
+                        "GetObject");
+        method.append(objpath);
+        method.append(std::vector<std::string>({iface}));
+
+        auto methodResponseMsg = bus.call(method);
+        if (methodResponseMsg.is_method_error())
+        {
+            //TODO Retry or throw exception/log error?
+        }
+        methodResponseMsg.read(methodResponse);
+
+        auto host = methodResponse.begin()->first;
+        method = bus.new_method_call(
+                    host.c_str(),
+                    objpath.c_str(),
+                    "org.freedesktop.DBus.Properties",
+                    "Get");
+        method.append(iface);
+        method.append("Value");
+
+        methodResponseMsg = bus.call(method);
+        if (methodResponseMsg.is_method_error())
+        {
+            //TODO Retry or throw exception/log error?
+        }
+        methodResponseMsg.read(tach);
+
+        return tach;
+    }
+
+} // namespace presence
+} // namespace fan
+} // namespace phosphor
