293477d4650314935a3741c547efcb36d5efb340
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 15:39:36 -0600

    Add fan presence application framework
    Add class framework for detecting fans by tach
    
    Change-Id: I45295fd6bcd81c62cef36320dfbc4f0da6092557
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ tach_sensor.cpp
@@ -0,0 +1,18 @@
+#include "tach_sensor.hpp"
+
+
+namespace phosphor
+{
+namespace fan
+{
+namespace presence
+{
+
+bool TachSensor::isPresent()
+{
+    return false;
+}
+
+} // namespace presence
+} // namespace fan
+} // namespace phosphor
