223b20429aa3eb74389f7faff1008caf0b80fd6c
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 16:39:46 -0600

    Create fan enclosure objects
    For tach feedback based presence detected fans, a fan enclosure object
    is created from the properties given within the yaml file.
    
    Change-Id: I698dd7c8d05863aaaab4dcb5d4c696b15fdf941d
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan-enclosure.cpp
+++ fan-enclosure.cpp
@@ -8,6 +8,10 @@ namespace fan
 namespace presence
 {
 
+    void FanEnclosure::addInventory()
+    {
+        //TODO Add this fan to inventory
+    }
 
 } // namespace presence
 } // namespace fan
--- tach-detect.cpp
+++ tach-detect.cpp
@@ -1,9 +1,27 @@
 #include <vector>
 #include <sdbusplus/bus.hpp>
+#include "fan-enclosure.hpp"
+#include "fan-detect-defs.hpp"
+
 
 int main(void)
 {
     auto bus = sdbusplus::bus::new_default();
+
+    std::vector<std::shared_ptr<phosphor::fan::presence::FanEnclosure>> fans;
+
+    for (auto const &detectMap: fanDetectMap)
+    {
+        if (detectMap.first == "tach")
+        {
+            for (auto const &fanProp: detectMap.second)
+            {
+                auto fan = std::make_shared<
+                    phosphor::fan::presence::FanEnclosure>(bus,
+                                                           fanProp);
+            }
+        }
+    }
 
     while (true) //TODO Only necessary while powered on
     {
