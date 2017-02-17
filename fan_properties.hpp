533ddd8c0b4a567a8313e421390ecf835e509696
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 16:39:46 -0600

    Create fan enclosure objects
    For tach feedback based presence detected fans, a fan enclosure object
    is created from the properties given within the yaml file.
    
    Change-Id: I698dd7c8d05863aaaab4dcb5d4c696b15fdf941d
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ fan_properties.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+namespace phosphor
+{
+namespace fan
+{
+
+struct Properties
+{
+    std::string inv;
+    std::string desc;
+    std::vector<std::string> sensors;
+
+    // Needed for inserting elements into sets
+    bool operator<(const Properties& right) const
+    {
+        if (inv == right.inv)
+        {
+            return sensors < right.sensors;
+        }
+        return inv < right.inv;
+    }
+};
+
+}
+}
