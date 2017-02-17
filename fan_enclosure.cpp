d7aa8d9c631311cbb9455aca2003ebd77bdb306c
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 17:48:56 -0600

    Enable tach signal handler
    Register and handle tach change signals for each tach sensor.
    Inventory would be updated when all tach sensors for a specific fan
    enclosure have a tach value of zero.
    
    Change-Id: Idd3f53c29c68c6171d858e616fbfe868f30a4ea9
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.cpp
+++ fan_enclosure.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "fan_enclosure.hpp"
 
 
@@ -12,6 +13,19 @@ void FanEnclosure::addInventory()
 {
     //TODO Add this fan to inventory
 }
+
+    void FanEnclosure::updInventory()
+    {
+        auto presPred = [](auto const& s) {return s->isPresent();};
+        // Determine if all sensors show fan is not present
+        auto isPresent = std::any_of(FanEnclosure::sensors.begin(),
+                                     FanEnclosure::sensors.end(),
+                                     presPred);
+        if (!isPresent)
+        {
+            //TODO Update inventory for this fan
+        }
+    }
 
     void FanEnclosure::addSensor(
         std::unique_ptr<phosphor::fan::presence::Sensor>&& sensor)
--- tach_sensor.cpp
+++ tach_sensor.cpp
@@ -1,4 +1,6 @@
+#include <sdbusplus/exception.hpp>
 #include "tach_sensor.hpp"
+#include "fan_enclosure.hpp"
 
 
 namespace phosphor
@@ -11,6 +13,44 @@ namespace presence
 bool TachSensor::isPresent()
 {
     return (tach != 0);
+}
+
+// Tach signal callback handler
+int TachSensor::handleTachChangeSignal(sd_bus_message* msg,
+                                       void* usrData,
+                                       sd_bus_error* err)
+{
+    auto sdbpMsg = sdbusplus::message::message(msg);
+    static_cast<TachSensor*>(usrData)->handleTachChange(sdbpMsg, err);
+    return 0;
+}
+
+void TachSensor::handleTachChange(sdbusplus::message::message& sdbpMsg,
+                                  sd_bus_error* err)
+{
+    try
+    {
+        std::string msgSensor;
+        std::map<std::string, sdbusplus::message::variant<int64_t>> msgData;
+        sdbpMsg.read(msgSensor, msgData);
+        // Find interface with value property
+        if (msgSensor.compare("xyz.openbmc_project.Sensor.Value") == 0)
+        {
+            // Find the 'Value' property containing tach
+            auto valPropMap = msgData.find("Value");
+            if (valPropMap != msgData.end())
+            {
+                tach = sdbusplus::message::variant_ns::get<int64_t>(
+                    valPropMap->second);
+            }
+        }
+        // Update inventory according to latest tach reported
+        fanEnc->updInventory();
+    }
+    catch (const sdbusplus::internal_exception_t& e)
+    {
+        //TODO Log exception to journal
+    }
 }
 
 } // namespace presence
