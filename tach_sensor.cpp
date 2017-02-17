4676cc446100c03afa08cd317adb2c388d1bb25c
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 17:48:56 -0600

    Enable tach signal handler
    Register and handle tach change signals for each tach sensor.
    Inventory would be updated when all tach sensors for a specific fan
    enclosure have a tach value of zero.
    
    Change-Id: Idd3f53c29c68c6171d858e616fbfe868f30a4ea9
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- tach_sensor.cpp
+++ tach_sensor.cpp
@@ -1,4 +1,6 @@
+#include <sdbusplus/exception.hpp>
 #include "tach_sensor.hpp"
+#include "fan_enclosure.hpp"
 
 
 namespace phosphor
@@ -11,6 +13,34 @@ namespace presence
     bool TachSensor::isPresent()
     {
         return false;
+    }
+
+    // Tach signal callback handler
+    int TachSensor::handleTachChangeSignal(sd_bus_message* msg,
+                                void* usrData,
+                                sd_bus_error* err)
+    {
+        return static_cast<TachSensor*>(usrData)->handleTachChange(msg, err);
+    }
+
+    int TachSensor::handleTachChange(sd_bus_message* msg, sd_bus_error* err)
+    {
+        try
+        {
+            int64_t curTach{};
+            auto sdbpMsg = sdbusplus::message::message(msg);
+            sdbpMsg.read(curTach);
+            tach = curTach;
+
+            fanEnc->updInventory();
+        }
+        catch (sdbusplus::internal_exception_t& e)
+        {
+            sd_bus_error_set_const(err, e.name(), e.description());
+            return -EINVAL;
+        }
+
+        return 0;
     }
 
 } // namespace presence
