8865c6d0b8ad5baf54b5fc14480bcfce6ba61982
Matthew Barth <msbarth@us.ibm.com>
2017-02-17 17:10:19 -0600

    Add tach sensors to each fan enclosure
    Create a tach sensor instance for each sensor listed within a fan
    enclosure that uses 'tach' based presence detection. Each tach sensor
    has a pointer to the fan it's associated with and is added to the list of
    sensors for that fan enclosure.
    
    Change-Id: I9a83ec52d1a5d01e39702e185336a09edeb4d158
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.cpp
+++ fan_enclosure.cpp
@@ -13,6 +13,12 @@ void FanEnclosure::addInventory()
     //TODO Add this fan to inventory
 }
 
+void FanEnclosure::addSensor(
+    std::unique_ptr<phosphor::fan::presence::Sensor>&& sensor)
+{
+    FanEnclosure::sensors.push_back(std::move(sensor));
+}
+
 } // namespace presence
 } // namespace fan
 } // namespace phosphor
