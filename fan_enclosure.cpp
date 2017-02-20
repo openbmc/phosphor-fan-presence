d869be32730d9538c5b9612f525f7d0e4c12cd28
Matthew Barth <msbarth@us.ibm.com>
2017-02-20 16:01:21 -0600

    Function to setup fan inventory object map
    The getObjectMap() function constructs the object map needed by
    inventory manager for the object path, interface, and properties of a
    fan.
    
    Change-Id: I8cce5cd611d3a0f7a2079b81f280b261aee486e2
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.cpp
+++ fan_enclosure.cpp
@@ -9,29 +9,42 @@ namespace fan
 namespace presence
 {
 
+ObjectMap FanEnclosure::getObjectMap()
+{
+    ObjectMap invObj;
+    InterfaceMap invIntf;
+    PropertyMap invProp;
+    auto presPred = [](auto const& s) {return s->isPresent();};
+    // Determine if all sensors show fan is not present
+    auto isPresent = std::any_of(sensors.begin(),
+                                 sensors.end(),
+                                 presPred);
+    invProp.emplace("Present", isPresent);
+    invProp.emplace("PrettyName", fanDesc);
+    invIntf.emplace("xyz.openbmc_project.Inventory.Item", invProp);
+    Object fanInvPath = invPath;
+    invObj.emplace(fanInvPath, invIntf);
+
+    return invObj;
+}
+
 void FanEnclosure::addInventory()
 {
     //TODO Add this fan to inventory
 }
 
-    void FanEnclosure::updInventory()
-    {
-        auto presPred = [](auto const& s) {return s->isPresent();};
-        // Determine if all sensors show fan is not present
-        auto isPresent = std::any_of(FanEnclosure::sensors.begin(),
-                                     FanEnclosure::sensors.end(),
-                                     presPred);
-        if (!isPresent)
-        {
-            //TODO Update inventory for this fan
-        }
-    }
+void FanEnclosure::updInventory()
+{
+    //Get inventory object for this fan
+    ObjectMap invObj = getObjectMap();
+    //TODO Update inventory for this fan
+}
 
-    void FanEnclosure::addSensor(
-        std::unique_ptr<phosphor::fan::presence::Sensor>&& sensor)
-    {
-        FanEnclosure::sensors.push_back(std::move(sensor));
-    }
+void FanEnclosure::addSensor(
+    std::unique_ptr<phosphor::fan::presence::Sensor>&& sensor)
+{
+    FanEnclosure::sensors.push_back(std::move(sensor));
+}
 
 } // namespace presence
 } // namespace fan
