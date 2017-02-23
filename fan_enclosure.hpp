546abf9c4e05ac4ef2c0c96443f2816cf170b165
Matthew Barth <msbarth@us.ibm.com>
2017-02-23 10:16:57 -0600

    Only update inventory when presence state changes
    When the presence state of a fan enclosure, determined from all the
    sensors part of that fan enclosure, changes then update inventory.
    
    Change-Id: Ie80e83fa7d0200239ced7b9d2ef84664e599e8ca
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.hpp
+++ fan_enclosure.hpp
@@ -23,6 +23,13 @@ namespace fan
 namespace presence
 {
 
+typedef enum presenceState
+{
+    NOT_PRESENT,
+    PRESENT,
+    UNKNOWN
+} presenceState;
+
 class FanEnclosure
 {
     public:
@@ -52,10 +59,12 @@ class FanEnclosure
         const std::string invPath;
         const std::string fanDesc;
         std::vector<std::unique_ptr<Sensor>> sensors;
+        presenceState presState = UNKNOWN;
 
+        presenceState getCurPresState();
         //TODO Move getInvService() to a utility type file
         std::string getInvService();
-        ObjectMap getObjectMap();
+        ObjectMap getObjectMap(const bool curPresState);
 
 };
 
