d0445f07bca6d4b95cc8f73d9259615f7d8b5cb9
Matthew Barth <msbarth@us.ibm.com>
2017-03-01 11:17:00 -0600

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Resolves openbmc/openbmc#959
    
    Change-Id: If58d78a39fb08251a34a88c2b6340c9fa33d2569
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_properties.hpp
+++ fan_properties.hpp
@@ -10,6 +10,10 @@ namespace phosphor
 namespace fan
 {
 
+/**
+ * @brief Fan enclosure properties
+ * @details Contains the inventory path, description and list of sensors
+ */
 using Properties = std::tuple<std::string,
                               std::string,
                               std::vector<std::string>>;
