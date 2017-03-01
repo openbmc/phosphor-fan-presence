bd94d5db6c6d793417c9e669424a911ea3eff4e4
Matthew Barth <msbarth@us.ibm.com>
2017-03-01 11:17:00 -0600

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Change-Id: If58d78a39fb08251a34a88c2b6340c9fa33d2569
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.hpp
+++ fan_enclosure.hpp
@@ -23,6 +23,13 @@ using Object = sdbusplus::message::object_path;
 // Association between object and the interface
 using ObjectMap = std::map<Object, InterfaceMap>;
 
+/**
+ * @class FanEnclosure
+ * @brief OpenBMC fan enclosure inventory presence implementation
+ * @details Inventory is based on the fan enclosure being present or not. This
+ * class represents that fan enclosure and updates its presences status within
+ * its inventory object based on the status of all its sensors.
+ */
 class FanEnclosure
 {
     public:
@@ -33,6 +40,12 @@ class FanEnclosure
         FanEnclosure& operator=(FanEnclosure&&) = default;
         ~FanEnclosure() = default;
 
+        /**
+         * @brief Constructs Fan Enclosure Object
+         *
+         * @param[in] bus - Dbus bus object
+         * @param[in] fanProp - Fan enclosure properties
+         */
         FanEnclosure(sdbusplus::bus::bus& bus,
                      const phosphor::fan::Properties& fanProp) :
                         bus(bus),
@@ -43,20 +56,51 @@ class FanEnclosure
             updInventory();
         }
 
+        /**
+         * @brief Update inventory when the determined presence of this fan
+         * enclosure has changed
+         */
         void updInventory();
+        /**
+         * @brief Add a sensor association to this fan enclosure
+         *
+         * @param[in] sensor - Sensor associated to this fan enclosure
+         */
         void addSensor(
             std::unique_ptr<phosphor::fan::presence::Sensor>&& sensor);
 
     private:
+        /** @brief Connection for sdbusplus bus */
         sdbusplus::bus::bus& bus;
+        /** @brief Inventory path for this fan enclosure */
         const std::string invPath;
+        /** @brief Description used as 'PrettyName' on inventory object */
         const std::string fanDesc;
+        /** @brief List of sensors associated with this fan enclosure */
         std::vector<std::unique_ptr<phosphor::fan::presence::Sensor>> sensors;
+        /** @brief Last known presence state of this fan enclosure */
         bool presState = false;
 
+        /**
+         * @brief Get the current presence state based on all sensors
+         *
+         * @return Current presence state determined from all sensors
+         */
         bool getCurPresState();
         //TODO Move getInvService() to a utility type file
+        /**
+         * @brief Get the inventory service name from the mapper object
+         *
+         * @return The inventory manager service name
+         */
         std::string getInvService();
+        /**
+         * @brief Construct the inventory object map
+         *
+         * @param[in] Current presence state
+         *
+         * @return The inventory object map to update inventory
+         */
         ObjectMap getObjectMap(const bool& curPresState);
 
 };
--- fan_properties.hpp
+++ fan_properties.hpp
@@ -9,10 +9,16 @@ namespace phosphor
 namespace fan
 {
 
+/**
+ * @brief Fan enclosure properties
+ */
 struct Properties
 {
+    /** @brief Inventory path for a fan enclosure */
     std::string inv;
+    /** @brief Description for a fan enclosure added to inventory object */
     std::string desc;
+    /** @brief List of sensors associated with a fan enclosure */
     std::vector<std::string> sensors;
 
     // Needed for inserting elements into sets
@@ -26,5 +32,5 @@ struct Properties
     }
 };
 
-}
-}
+} // namespace fan
+} // namespace phosphor
