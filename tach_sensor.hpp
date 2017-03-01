67ffaf3778af6f93f3050b3247950bb527e173a3
Matthew Barth <msbarth@us.ibm.com>
2017-03-01 11:17:00 -0600

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Change-Id: If58d78a39fb08251a34a88c2b6340c9fa33d2569
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- tach_sensor.hpp
+++ tach_sensor.hpp
@@ -12,6 +12,12 @@ namespace fan
 namespace presence
 {
 
+/**
+ * @class TachSensor
+ * @brief OpenBMC Tach feedback sensor presence implementation
+ * @details Derived sensor type that uses the tach feedback value to determine
+ * the presence of the fan enclosure that contains this sensor
+ */
 class TachSensor : public Sensor
 {
     public:
@@ -22,6 +28,13 @@ class TachSensor : public Sensor
         TachSensor& operator=(TachSensor&&) = default;
         ~TachSensor() = default;
 
+        /**
+         * @brief Constructs Tach Sensor Object
+         *
+         * @param[in] bus - Dbus bus object
+         * @param[in] id - ID name of this sensor
+         * @param[in] fanEnc - Reference to the fan enclosure with this sensor
+         */
         TachSensor(sdbusplus::bus::bus& bus,
                    const std::string& match,
                    auto fanEnc) : Sensor(id, fanEnc),
@@ -34,19 +47,41 @@ class TachSensor : public Sensor
             // Nothing to do here
         }
 
+        /**
+         * @brief Determine the presence of this sensor using the tach feedback
+         *
+         * @return Presence state based on tach feedback
+         */
         bool isPresent();
 
     private:
+        /** @brief Connection for sdbusplus bus */
         sdbusplus::bus::bus& bus;
+        /** @brief Match string on the hwmon tach signal for this sensor ID */
         const std::string match;
+        /** @brief Used to subscribe to dbus signals */
         sdbusplus::server::match::match tachSignal;
+        /** @brief Tach speed value given from the signal */
         int64_t tach = 0;
 
-        // Tach signal callback handler
+        /**
+         * @brief Callback function on tach change signals
+         *
+         * @param[out] msg - Data associated with the subscribed signal
+         * @param[out] data - Pointer to this tach sensor object instance
+         * @param[out] err - Contains any sdbus error reference if occurred
+         *
+         * @return 0
+         */
         static int handleTachChangeSignal(sd_bus_message* msg,
                                           void* data,
                                           sd_bus_error* err);
-
+        /**
+         * @brief Determine & handle when the signal was a tach change
+         *
+         * @param[in] msg - Expanded sdbusplus message data
+         * @param[in] err - Contains any sdbus error reference if occurred
+         */
         void handleTachChange(sdbusplus::message::message& msg,
                               sd_bus_error* err);
 
