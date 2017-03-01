8bc14216440a09ae96b40d6d14cdde3dadef93fd
Matthew Barth <msbarth@us.ibm.com>
2017-03-01 11:17:00 -0600

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Resolves openbmc/openbmc#959
    
    Change-Id: If58d78a39fb08251a34a88c2b6340c9fa33d2569
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- fan_enclosure.hpp
+++ fan_enclosure.hpp
@@ -23,6 +23,9 @@ namespace fan
 namespace presence
 {
 
+/**
+ * @brief Specifies the defined presence states of a fan enclosure
+ */
 typedef enum presenceState
 {
     NOT_PRESENT,
@@ -30,6 +33,13 @@ typedef enum presenceState
     UNKNOWN
 } presenceState;
 
+/**
+ * @class FanEnclosure
+ * @brief OpenBMC fan enclosure inventory presence implementation
+ * @details Inventory is based on the fan enclosure being present or not. This
+ * class represents that fan enclosure and updates its presences status within
+ * its inventory object based on the status of all its sensors.
+ */
 class FanEnclosure
 {
     public:
@@ -40,6 +50,12 @@ class FanEnclosure
         FanEnclosure& operator=(FanEnclosure&&) = delete;
         ~FanEnclosure() = default;
 
+        /**
+         * @brief Constructs Fan Enclosure Object
+         *
+         * @param[in] bus - Dbus bus object
+         * @param[in] fanProp - Fan enclosure properties
+         */
         FanEnclosure(sdbusplus::bus::bus& bus,
                      const phosphor::fan::Properties& fanProp) :
                         bus(bus),
@@ -50,20 +66,51 @@ class FanEnclosure
             updInventory();
         }
 
+        /**
+         * @brief Update inventory when the determined presence of this fan
+         * enclosure has changed
+         */
         void updInventory();
+        /**
+         * @brief Add a sensor association to this fan enclosure
+         *
+         * @param[in] sensor - Sensor associated to this fan enclosure
+         */
         void addSensor(
             std::unique_ptr<Sensor>&& sensor);
 
     private:
+        /** @brief Connection for sdbusplus bus */
         sdbusplus::bus::bus& bus;
+        /** @brief Inventory path for this fan enclosure */
         const std::string invPath;
+        /** @brief Description used as 'PrettyName' on inventory object */
         const std::string fanDesc;
+        /** @brief List of sensors associated with this fan enclosure */
         std::vector<std::unique_ptr<Sensor>> sensors;
+        /** @brief Last known presence state of this fan enclosure */
         presenceState presState = UNKNOWN;
 
+        /**
+         * @brief Get the current presence state based on all sensors
+         *
+         * @return Current presence state determined from all sensors
+         */
         presenceState getCurPresState();
         //TODO Move getInvService() to a utility type file
+        /**
+         * @brief Get the inventory service name from the mapper object
+         *
+         * @return The inventory manager service name
+         */
         std::string getInvService();
+        /**
+         * @brief Construct the inventory object map
+         *
+         * @param[in] Current presence state
+         *
+         * @return The inventory object map to update inventory
+         */
         ObjectMap getObjectMap(const bool curPresState);
 
 };
--- tach_sensor.hpp
+++ tach_sensor.hpp
@@ -12,6 +12,12 @@ namespace fan
 namespace presence
 {
 
+/**
+ * @class TachSensor
+ * @brief OpenBMC Tach feedback sensor presence implementation
+ * @details Derived sensor type that uses the tach feedback value to determine
+ * the presence of the fan enclosure that contains this sensor
+ */
 class TachSensor : public Sensor
 {
     public:
@@ -22,6 +28,13 @@ class TachSensor : public Sensor
         TachSensor& operator=(TachSensor&&) = delete;
         ~TachSensor() = default;
 
+        /**
+         * @brief Constructs Tach Sensor Object
+         *
+         * @param[in] bus - Dbus bus object
+         * @param[in] id - ID name of this sensor
+         * @param[in] fanEnc - Reference to the fan enclosure with this sensor
+         */
         TachSensor(
             sdbusplus::bus::bus& bus,
             const std::string& match,
@@ -37,18 +50,39 @@ class TachSensor : public Sensor
             // Nothing to do here
         }
 
+        /**
+         * @brief Determine the presence of this sensor using the tach feedback
+         *
+         * @return Presence state based on tach feedback
+         */
         bool isPresent();
 
     private:
+        /** @brief Connection for sdbusplus bus */
         sdbusplus::bus::bus& bus;
+        /** @brief Used to subscribe to dbus signals */
         sdbusplus::server::match::match tachSignal;
+        /** @brief Tach speed value given from the signal */
         int64_t tach = 0;
 
-        // Tach signal callback handler
+        /**
+         * @brief Callback function on tach change signals
+         *
+         * @param[out] msg - Data associated with the subscribed signal
+         * @param[out] data - Pointer to this tach sensor object instance
+         * @param[out] err - Contains any sdbus error reference if occurred
+         *
+         * @return 0
+         */
         static int handleTachChangeSignal(sd_bus_message* msg,
                                           void* data,
                                           sd_bus_error* err);
-
+        /**
+         * @brief Determine & handle when the signal was a tach change
+         *
+         * @param[in] msg - Expanded sdbusplus message data
+         * @param[in] err - Contains any sdbus error reference if occurred
+         */
         void handleTachChange(sdbusplus::message::message& msg,
                               sd_bus_error* err);
 
