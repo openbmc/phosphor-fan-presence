5c15b797ffafd36eceecfbf250bd0eead39103dd
Matthew Barth <msbarth@us.ibm.com>
2017-03-01 11:17:00 -0600

    Documentation only, no functional change
    Added copyrights and comments within headers
    
    Resolves openbmc/openbmc#959
    
    Change-Id: If58d78a39fb08251a34a88c2b6340c9fa33d2569
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- sensor_base.hpp
+++ sensor_base.hpp
@@ -8,7 +8,14 @@ namespace fan
 namespace presence
 {
 
+// Forward declare FanEnclosure
 class FanEnclosure;
+/**
+ * @class Sensor
+ * @brief Base sensor implementation to be extended
+ * @details A type of presence detection sensor would extend this to override
+ * how presences is determined by the fan enclosure containing that type
+ */
 class Sensor
 {
     public:
@@ -19,6 +26,12 @@ class Sensor
         Sensor& operator=(Sensor&&) = delete;
         virtual ~Sensor() = default;
 
+        /**
+         * @brief Constructs Sensor Object
+         *
+         * @param[in] id - ID name of this sensor
+         * @param[in] fanEnc - Reference to the fan enclosure with this sensor
+         */
         Sensor(const std::string& id,
                FanEnclosure& fanEnc) :
             id(id),
@@ -27,10 +40,16 @@ class Sensor
             //Nothing to do here
         }
 
+        /**
+         * @brief Presence function that must be implemented within the derived
+         * type of sensor's implementation on how presence is determined
+         */
         virtual bool isPresent() = 0;
 
     protected:
+        /** @brief ID name of this sensor */
         const std::string id;
+        /** @brief Reference to the fan enclosure containing this sensor */
         FanEnclosure& fanEnc;
 
 };
