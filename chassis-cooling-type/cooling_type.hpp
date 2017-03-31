b8dd75700ec587cca39dc78ebf9b520c9d2090dd
Brandon Wyman <bjwyman@gmail.com>
2017-03-31 18:13:21 -0500

    Fill in shell functions/files for cooling type.
    Change-Id: I8d18d3141e955adeffdd8122ebb17549c88edd77
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- chassis-cooling-type/cooling_type.hpp
+++ chassis-cooling-type/cooling_type.hpp
@@ -6,7 +6,55 @@ namespace chassis
 {
 namespace cooling
 {
+constexpr auto CHASSIS_PATH = "/xyz/openbmc_project/inventory/system/chassis";
+constexpr auto CHASSIS_BUSNAME = "xyz.openbmc_project.Inventory.System.Chassis";
+
+class CoolingType
+{
+    public:
+        CoolingType() = delete;
+        ~CoolingType();
+
+        /**
+         * @brief Constructs Cooling Type Object
+         *
+         * @param[in] bus - Dbus bus object
+         */
+        CoolingType(sdbusplus::bus::bus& bus) :
+                    gpioFd(0)
+        {
+            updateInventory();
+        }
+
+        /**
+         * @brief Sets airCooled to true.
+         */
+        void setAirCooled();
+        /**
+         * @brief Sets waterCooled to true.
+         */
+        void setWaterCooled();
+        /**
+         * @brief Updates the inventory properties for CoolingType.
+         */
+        void updateInventory();
+        /**
+         * @brief Setup the GPIO device for reading cooling type.
+         *
+         * @param[in] std::string - Path to the GPIO device file to read
+         */
+        void setupGpio(std::string);
+
+    private:
+        // File descriptor for the GPIO file we are going to read.
+        int gpioFd = 0;
+        const char* gpioPath;
+        bool airCooled = false;
+        bool waterCooled = false;
+};
 
 }
 }
 }
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
