ec2f1629dcf02de50d79a60e1616e293ea744838
Brandon Wyman <bjwyman@gmail.com>
2017-03-31 18:13:21 -0500

    Fill in shell functions/files for cooling type.
    Change-Id: I8d18d3141e955adeffdd8122ebb17549c88edd77
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

---
+++ cooling-type/cooling_type.cpp
@@ -0,0 +1,59 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sdbusplus/bus.hpp>
+#include <phosphor-logging/log.hpp>
+#include "cooling_type.hpp"
+
+namespace phosphor
+{
+namespace cooling
+{
+namespace type
+{
+
+void CoolingType::setAirCooled()
+{
+    airCooled = true;
+}
+
+void CoolingType::setWaterCooled()
+{
+    waterCooled = true;
+}
+
+void CoolingType::setupGpio(const std::string& gpioPath)
+{
+    using namespace phosphor::logging;
+
+    gpioFd = open(gpioPath.c_str(), O_RDONLY);
+    if (gpioFd.is_open())
+    {
+        auto rc = 0;//libevdev_new_from_fd(gpiofd, &gpioDev);//FIXME
+        if (rc < 0)
+        {
+            log<level::ERR>("Failed to get file descriptor for ",
+                            entry("path=%s strerror(%s)\n", gpioPath.c_str(),
+                                  strerror(-rc)));
+        }
+
+        //TODO - more to go here?
+    }
+    else
+    {
+        perror("Failed to open GPIO file device");
+    }
+
+}
+
+void CoolingType::updateInventory(const std::string& objpath)
+{
+    //TODO
+    //     setProperty(bus, ..., "AirCooled");
+    //     setProperty(bus, ..., "WaterCooled");
+}
+
+}
+}
+}
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
