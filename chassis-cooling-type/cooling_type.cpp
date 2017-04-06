0bd74164a653f9346c7784b49217d95bde3e6f64
Brandon Wyman <bjwyman@gmail.com>
2017-04-06 15:05:18 -0500

    Implement updating the inventory properties
    Implement code needed to update the chassis AirCooled and
    WaterCooled properties.
    
    Change-Id: I9a67d1129e99f841200241e2daae08fb6119d9d3
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- chassis-cooling-type/cooling_type.cpp
+++ chassis-cooling-type/cooling_type.cpp
@@ -2,10 +2,21 @@
 #include "fcntl.h"
 #include "unistd.h"
 #include <sdbusplus/bus.hpp>
+#include <phosphor-logging/log.hpp>
+#include "utility.hpp"
 #include "argument.hpp"
 #include "cooling_type.hpp"
 
-using namespace phosphor::chassis::cooling;
+namespace phosphor
+{
+namespace chassis
+{
+namespace cooling
+{
+
+//TODO Should get these from phosphor-inventory-manager config.h
+constexpr auto INVENTORY_PATH = "/xyz/openbmc_project/inventory";
+constexpr auto INVENTORY_INTF = "xyz.openbmc_project.Inventory.Manager";
 
 CoolingType::~CoolingType()
 {
@@ -46,19 +57,64 @@ void CoolingType::setupGpio(std::string gpioPath)
     }
 }
 
+CoolingType::ObjectMap CoolingType::getObjectMap()
+{
+    ObjectMap invObj;
+    InterfaceMap invIntf;
+    PropertyMap invProp;
+
+    invProp.emplace("AirCooled", airCooled);
+    invProp.emplace("WaterCooled", waterCooled);
+    invIntf.emplace("xyz.openbmc_project.Inventory.Decorator.CoolingType", 
+                    std::move(invProp));
+    Object invPath("/xyz/openbmc_project/inventory/chassis");
+    invObj.emplace(std::move(invPath), std::move(invIntf));
+
+    return invObj;
+}
+
 void CoolingType::updateInventory(void)
 {
-    //TODO
-    //     setProperty(bus, ..., "AirCooled");
-    //     setProperty(bus, ..., "WaterCooled");
+    using namespace phosphor::logging;
+
+    ObjectMap invObj = getObjectMap();
+
+    std::string invService;
+    try
+    {
+        invService = phosphor::fan::presence::getInvService(bus);
+    }
+    catch (const std::runtime_error& err)
+    {
+        log<level::ERR>(err.what());
+        return;
+    }
+
+    // Update inventory
+    auto invMsg = bus.new_method_call(invService.c_str(),
+                                      INVENTORY_PATH,
+                                      INVENTORY_INTF,
+                                      "Notify");
+    invMsg.append(std::move(invObj));
+    auto invMgrResponseMsg = bus.call(invMsg);
+    if (invMgrResponseMsg.is_method_error())
+    {
+        log<level::ERR>(
+            "Error in inventory manager call to update inventory");
+        return;
+    }
 }
 
 // Utility function to find the device string for a given pin name.
 std::string findGpio(std::string pinName)
 {
-    std::string path="/dev/null";
+    std::string path = "/dev/null";
     //TODO
     return path;
+}
+
+}
+}
 }
 
 int main(int argc, char* argv[])
@@ -69,32 +125,34 @@ int main(int argc, char* argv[])
     if (argc < 2)
     {
         std::cerr << std::endl << "Too few arguments" << std::endl;
+        options->usage(argv);
     }
     else
     {
         auto bus = sdbusplus::bus::new_default();
-        CoolingType coolingType(bus);
+        auto coolingType = std::make_unique<phosphor::chassis::cooling::CoolingType>
+                           (bus);
 
         auto gpiopin = (*options)["gpio"];
         if (gpiopin != ArgumentParser::empty_string)
         {
-            auto gpiopath = findGpio(gpiopin);
-            coolingType.setupGpio(gpiopath);
+            auto gpiopath = phosphor::chassis::cooling::findGpio(gpiopin);
+            coolingType->setupGpio(gpiopath);
         }
 
         auto air = (*options)["air"];
         if (air != ArgumentParser::empty_string)
         {
-            coolingType.setAirCooled();
+            coolingType->setAirCooled();
         }
 
         auto water = (*options)["water"];
         if (water != ArgumentParser::empty_string)
         {
-            coolingType.setWaterCooled();
+            coolingType->setWaterCooled();
         }
 
-        coolingType.updateInventory();
+        coolingType->updateInventory();
 
         rc = 0;
     }
