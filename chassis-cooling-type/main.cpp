e36281a55b4648a917516b36890dada604511ece
Brandon Wyman <bjwyman@gmail.com>
2017-04-06 15:05:18 -0500

    Implement updating the inventory properties
    Implement code needed to update the chassis AirCooled and
    WaterCooled properties.
    
    Change-Id: I9a67d1129e99f841200241e2daae08fb6119d9d3
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

---
+++ chassis-cooling-type/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <memory>
+#include <sdbusplus/bus.hpp>
+#include <phosphor-logging/log.hpp>
+#include <utility.hpp>
+#include "argument.hpp"
+#include "cooling_type.hpp"
+
+// Utility function to find the device string for a given pin name.
+std::string findGpio(std::string pinName)
+{
+    std::string path = "/dev/null";
+    //TODO
+    return path;
+}
+
+int main(int argc, char* argv[])
+{
+    int rc = -1;
+    auto options = std::make_unique<ArgumentParser>(argc, argv);
+
+    if (argc < 2)
+    {
+        std::cerr << std::endl << "Too few arguments" << std::endl;
+        options->usage(argv);
+    }
+    else
+    {
+        auto bus = sdbusplus::bus::new_default();
+        auto coolingType = std::make_unique<phosphor::chassis::cooling::CoolingType>
+                           (bus);
+
+        auto gpiopin = (*options)["gpio"];
+        if (gpiopin != ArgumentParser::empty_string)
+        {
+            auto gpiopath = findGpio(gpiopin);
+            coolingType->setupGpio(gpiopath);
+        }
+
+        auto air = (*options)["air"];
+        if (air != ArgumentParser::empty_string)
+        {
+            coolingType->setAirCooled();
+        }
+
+        auto water = (*options)["water"];
+        if (water != ArgumentParser::empty_string)
+        {
+            coolingType->setWaterCooled();
+        }
+
+        coolingType->updateInventory();
+
+        rc = 0;
+    }
+
+    return rc;
+}
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
