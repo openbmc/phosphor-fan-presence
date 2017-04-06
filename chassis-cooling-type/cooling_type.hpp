ddead81766782ec60a3b2fbec7fc137238b181a9
Brandon Wyman <bjwyman@gmail.com>
2017-04-06 15:05:18 -0500

    Implement updating the inventory properties
    Implement code needed to update the chassis AirCooled and
    WaterCooled properties.
    
    Change-Id: I9a67d1129e99f841200241e2daae08fb6119d9d3
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- chassis-cooling-type/cooling_type.hpp
+++ chassis-cooling-type/cooling_type.hpp
@@ -14,38 +14,65 @@ constexpr auto CHASSIS_BUSNAME = "xyz.openbmc_project.Inventory.System.Chassis";
 
 class CoolingType
 {
+        using Property = std::string;
+        using Value = sdbusplus::message::variant<bool, int64_t, std::string>;
+        // Association between property and its value
+        using PropertyMap = std::map<Property, Value>;
+        using Interface = std::string;
+        // Association between interface and the dbus property
+        using InterfaceMap = std::map<Interface, PropertyMap>;
+        using Object = sdbusplus::message::object_path;
+        // Association between object and the interface
+        using ObjectMap = std::map<Object, InterfaceMap>;
+
     public:
         CoolingType() = delete;
-        ~CoolingType();
+        CoolingType(CoolingType&&) = default;
+        CoolingType& operator=(const CoolingType&) = delete;
+        CoolingType& operator=(CoolingType&&) = delete;
 
         /**
          * @brief Constructs Cooling Type Object
          *
          * @param[in] bus - Dbus bus object
          */
-        CoolingType(sdbusplus::bus::bus& bus) : 
-                    gpioFd(0)
+        CoolingType(sdbusplus::bus::bus& bus) :
+            bus(bus), gpioFd(0)
         {
             updateInventory();
         }
+
+        ~CoolingType();
 
         // Set airCooled to true.
         void setAirCooled();
         // Set waterCooled to true.
         void setWaterCooled();
-        // Update the inventory properties for cooling to the current values.
+        ///! Update the inventory properties for cooling to the current values.
         void updateInventory();
         // Setup the GPIO device for reading cooling type.
         void setupGpio(std::string);
 
     private:
+        /** @brief Connection for sdbusplus bus */
+        sdbusplus::bus::bus& bus;
         // File descriptor for the GPIO file we are going to read.
         int gpioFd;
         const char* gpioPath;
         bool airCooled = false;
         bool waterCooled = false;
+
+        /**
+         * @brief Construct the inventory object map for CoolingType.
+         *
+         * @return The inventory object map to update inventory
+         */
+        ObjectMap getObjectMap(void);
+
 };
 
 }
 }
 }
+
+
