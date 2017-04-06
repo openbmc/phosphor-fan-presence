929842656f637bc2829d72d716be998597569f1f
Brandon Wyman <bjwyman@gmail.com>
2017-04-06 15:05:18 -0500

    Implement updating the inventory properties
    Implement code needed to update the chassis AirCooled and
    WaterCooled properties.
    
    Change-Id: I9a67d1129e99f841200241e2daae08fb6119d9d3
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>

--- chassis-cooling-type/cooling_type.cpp
+++ chassis-cooling-type/cooling_type.cpp
@@ -1,12 +1,30 @@
+#include <fcntl.h>
+#include <unistd.h>
 #include <iostream>
-#include <sdbusplus/bus.hpp>
+#include <libevdev/libevdev.h>
+#include <phosphor-logging/log.hpp>
+#include "utility.hpp"
 #include "argument.hpp"
 #include "cooling_type.hpp"
 
-using namespace phosphor::chassis::cooling;
+namespace phosphor
+{
+namespace chassis
+{
+namespace cooling
+{
+
+//TODO Should get these from phosphor-inventory-manager config.h
+constexpr auto INVENTORY_PATH = "/xyz/openbmc_project/inventory";
+constexpr auto INVENTORY_INTF = "xyz.openbmc_project.Inventory.Manager";
 
 CoolingType::~CoolingType()
 {
+    if (gpioDev != nullptr)
+    {
+        libevdev_free(gpioDev);
+    }
+
     if (gpioFd > 0)
     {
         close(gpioFd);
@@ -24,80 +42,110 @@ void CoolingType::setWaterCooled()
     waterCooled = true;
 }
 
-void CoolingType::setupGpio(std::string gpioPath)
+void CoolingType::setupGpio(std::string gpioPath, unsigned int keycode)
 {
+    using namespace phosphor::logging;
+
     gpioFd = open(gpioPath.c_str(), O_RDONLY);
     if (gpioFd < 0)
     {
-        perror("Failed to open GPIO file device");
+        log<level::ERR>("Failed to open GPIO file device",
+                        entry("%s", gpioPath.c_str()));
+        throw 1;
     }
     else
     {
-        int rc = 0;//libevdev_new_from_fd(gpiofd, &gpioDev);//FIXME
+        int rc = libevdev_new_from_fd(gpioFd, &gpioDev);
         if (rc < 0)
         {
-            fprintf(stderr, "Failed to get file descriptor for %s (%s)\n", gpioPath.c_str(),
-                    strerror(-rc));
+            fprintf(stderr, "Failed to get file descriptor for %s (%s)\n",
+                    gpioPath.c_str(), strerror(-rc));
+            log<level::ERR>("Failed to get file descriptor for ",
+                            entry("path=%s strerror(%s)\n", gpioPath.c_str(),
+                                  strerror(-rc)));
+            throw 2;
         }
+        else
+        {
+            int value = 1;
+            int fetch_rc = libevdev_fetch_event_value(gpioDev, EV_KEY, keycode,
+                           &value);
+            if (0 == fetch_rc)
+            {
+                log<level::ERR>("Device does not support event type and code",
+                                entry("type=%d", EV_KEY),
+                                entry("code=%d", keycode));
+                throw 3;
+            }
+            else
+            {
+                std::cout << "State of keycode[" << keycode << "]: "
+                          << value << std::endl;
 
-        //TODO - more to go here?
+                // TODO: Issue #xyz - get value from parameters.
+                if (value > 0)
+                {
+                    setWaterCooled();
+                }
+                else
+                {
+                    setAirCooled();
+                }
+            }
+        }
     }
+}
+
+CoolingType::ObjectMap CoolingType::getObjectMap()
+{
+    ObjectMap invObj;
+    InterfaceMap invIntf;
+    PropertyMap invProp;
+
+    invProp.emplace("AirCooled", airCooled);
+    invProp.emplace("WaterCooled", waterCooled);
+    invIntf.emplace("xyz.openbmc_project.Inventory.Decorator.CoolingType",
+                    std::move(invProp));
+    Object invPath("/system/chassis");
+    invObj.emplace(std::move(invPath), std::move(invIntf));
+
+    return invObj;
 }
 
 void CoolingType::updateInventory(void)
 {
-    //TODO
-    //     setProperty(bus, ..., "AirCooled");
-    //     setProperty(bus, ..., "WaterCooled");
-}
+    using namespace phosphor::logging;
 
-// Utility function to find the device string for a given pin name.
-std::string findGpio(std::string pinName)
-{
-    std::string path="/dev/null";
-    //TODO
-    return path;
-}
+    ObjectMap invObj = getObjectMap();
 
-int main(int argc, char* argv[])
-{
-    int rc = -1;
-    auto options = std::make_unique<ArgumentParser>(argc, argv);
-
-    if (argc < 2)
+    std::string invService;
+    try
     {
-        std::cerr << std::endl << "Too few arguments" << std::endl;
+        invService = phosphor::fan::presence::getInvService(bus);
     }
-    else
+    catch (const std::runtime_error& err)
     {
-        auto bus = sdbusplus::bus::new_default();
-        CoolingType coolingType(bus);
-
-        auto gpiopin = (*options)["gpio"];
-        if (gpiopin != ArgumentParser::empty_string)
-        {
-            auto gpiopath = findGpio(gpiopin);
-            coolingType.setupGpio(gpiopath);
-        }
-
-        auto air = (*options)["air"];
-        if (air != ArgumentParser::empty_string)
-        {
-            coolingType.setAirCooled();
-        }
-
-        auto water = (*options)["water"];
-        if (water != ArgumentParser::empty_string)
-        {
-            coolingType.setWaterCooled();
-        }
-
-        coolingType.updateInventory();
-
-        rc = 0;
+        log<level::ERR>(err.what());
+        return;
     }
 
-    return rc;
+    // Update inventory
+    auto invMsg = bus.new_method_call(invService.c_str(),
+                                      INVENTORY_PATH,
+                                      INVENTORY_INTF,
+                                      "Notify");
+    invMsg.append(std::move(invObj));
+    auto invMgrResponseMsg = bus.call(invMsg);
+    if (invMgrResponseMsg.is_method_error())
+    {
+        log<level::ERR>(
+            "Error in inventory manager call to update inventory");
+        return;
+    }
+}
+
+}
+}
 }
 
 // vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
--- chassis-cooling-type/cooling_type.hpp
+++ chassis-cooling-type/cooling_type.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <libevdev/libevdev.h>
 
 namespace phosphor
 {
@@ -11,9 +12,23 @@ constexpr auto CHASSIS_BUSNAME = "xyz.openbmc_project.Inventory.System.Chassis";
 
 class CoolingType
 {
+        using Property = std::string;
+        using Value = sdbusplus::message::variant<bool, int64_t, std::string>;
+        // Association between property and its value
+        using PropertyMap = std::map<Property, Value>;
+        using Interface = std::string;
+        // Association between interface and the dbus property
+        using InterfaceMap = std::map<Interface, PropertyMap>;
+        using Object = sdbusplus::message::object_path;
+        // Association between object and the interface
+        using ObjectMap = std::map<Object, InterfaceMap>;
+
     public:
         CoolingType() = delete;
         ~CoolingType();
+        CoolingType(CoolingType&&) = default;
+        CoolingType& operator=(const CoolingType&) = delete;
+        CoolingType& operator=(CoolingType&&) = delete;
 
         /**
          * @brief Constructs Cooling Type Object
@@ -21,9 +36,9 @@ class CoolingType
          * @param[in] bus - Dbus bus object
          */
         CoolingType(sdbusplus::bus::bus& bus) :
-                    gpioFd(0)
+            bus(bus), gpioFd(0)
         {
-            updateInventory();
+            //updateInventory();//TODO: Issue #??? - someone default properties.
         }
 
         /**
@@ -43,14 +58,25 @@ class CoolingType
          *
          * @param[in] std::string - Path to the GPIO device file to read
          */
-        void setupGpio(std::string);
+        void setupGpio(std::string, unsigned int);
 
     private:
+        /** @brief Connection for sdbusplus bus */
+        sdbusplus::bus::bus& bus;
+        struct libevdev* gpioDev = nullptr;
         // File descriptor for the GPIO file we are going to read.
         int gpioFd = 0;
         const char* gpioPath;
         bool airCooled = false;
         bool waterCooled = false;
+
+        /**
+         * @brief Construct the inventory object map for CoolingType.
+         *
+         * @return The inventory object map to update inventory
+         */
+        ObjectMap getObjectMap(void);
+
 };
 
 }
