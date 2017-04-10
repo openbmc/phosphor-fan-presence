0fa9f9d8f3cfb2278781b9be0b589ef52e07b4db
Matt Spinler <spinler@us.ibm.com>
2017-04-10 13:58:08 -0500

    Fan control: Add types for use with generated data
    Change-Id: I9cb2037dc459516f577fc2bff91cccafea539eff
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ control/types.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+
+//Placeholder. Conditions are completely TBD.
+using Condition = bool;
+
+constexpr auto fanNamePos = 0;
+constexpr auto sensorListPos = 1;
+using FanDefinition = std::tuple<std::string, std::vector<std::string>>;
+
+constexpr auto zoneNumPos = 0;
+constexpr auto fullSpeedPos = 1;
+constexpr auto fanListPos = 2;
+using ZoneDefinition = std::tuple<unsigned int,
+                                  unsigned int,
+                                  std::vector<FanDefinition>>;
+
+constexpr auto conditionListPos = 0;
+constexpr auto zoneListPos = 1;
+using ZoneGroup = std::tuple<std::vector<Condition>,
+                             std::vector<ZoneDefinition>>;
+
+}
+}
+}
