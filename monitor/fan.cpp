8581ffb78d8d4cbd4b6a8e1c86653bbe335e218d
Matt Spinler <spinler@us.ibm.com>
2017-04-27 14:29:24 -0500

    Add Timer support to TachSensor
    Add a Timer object to the TachSensor class
    
    Change-Id: I419b5712de9e8e94f2a08de84d13170e44c33c7a
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- monitor/fan.cpp
+++ monitor/fan.cpp
@@ -45,8 +45,8 @@ Fan::Fan(sdbusplus::bus::bus& bus,
                                          std::get<sensorNameField>(s),
                                          std::get<hasTargetField>(s),
                                          std::get<startupTimeoutField>(def),
-                                         std::get<timeoutField>(def)));
-
+                                         std::get<timeoutField>(def),
+                                         events));
     }
 
 }
@@ -120,6 +120,16 @@ bool Fan::tooSlow(const TachSensor& sensor)
     return false;
 }
 
+
+void Fan::timerExpired(TachSensor* sensor)
+{
+    sensor->setFunctional(false);
+
+    //If the fan is currently functional, but too many
+    //contained sensors are now nonfunctional, update
+    //the whole fan nonfunctional.
+	//TODO
+}
 
 }
 }
