0b7799f06879a77fc02680b748dc1276ca4829ad
Matt Spinler <spinler@us.ibm.com>
2017-04-27 12:35:23 -0500

    Fan monitor data
    This commit contains the data that will be used by
    the monitor code.
    
    The generated.cpp file will later be generated during
    the build by a python script.
    
    Change-Id: I4dc4552ae8e58cd27478416888dd3363e7c2bf3f
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ monitor/types.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace phosphor
+{
+namespace fan
+{
+namespace monitor
+{
+
+constexpr auto sensorNameField = 0;
+constexpr auto hasTargetField = 1;
+
+using SensorDefinition = std::tuple<std::string, bool>;
+
+constexpr auto fanNameField = 0;
+constexpr auto timeoutField = 1;
+constexpr auto fanDeviationField = 2;
+constexpr auto numSensorFailsForNonfuncField = 3;
+constexpr auto sensorListField = 4;
+
+using FanDefinition = std::tuple<std::string,
+                                 size_t,
+                                 size_t,
+                                 size_t,
+                                 std::vector<SensorDefinition>>;
+
+}
+}
+}
