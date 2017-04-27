9b4e5490b4a5a49ebc08bc0d924ad77636625f6a
Matt Spinler <spinler@us.ibm.com>
2017-04-27 14:38:38 -0500

    Add timer start/stop support
    In Fan's tachChanged handler, start up the timer
    for a sensor if it is running too slow.  If it is
    within spec, stop the timer if running and make
    the sensor functional again if necessary.
    
    Change-Id: Ib18de2b69942d334da0cb8cd4cc4de8a2784efab
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- monitor/fan.cpp
+++ monitor/fan.cpp
@@ -63,7 +63,48 @@ void Fan::tachChanged()
 
 void Fan::tachChanged(TachSensor& sensor)
 {
-    //TODO
+    auto& timer = sensor.getTimer();
+    auto running = timer.running();
+
+    //If this sensor is out of range at this moment, start
+    //its timer, at the end of which the inventory
+    //for the fan may get updated to not functional.
+
+    //If this sensor is OK, put everything back into a good state.
+
+    if (outOfRange(sensor))
+    {
+        if (sensor.functional() && !running)
+        {
+            timer.start(sensor.getTimeout());
+        }
+    }
+    else
+    {
+        if (!sensor.functional())
+        {
+            sensor.setFunctional(true);
+        }
+
+        if (running)
+        {
+            timer.stop();
+        }
+
+        //If the fan was nonfunctional and enough sensors are now OK,
+        //the fan can go back to functional
+        if ((_functional != functionalState::FUNCTIONAL) &&
+            !tooManySensorsNonfunctional())
+        {
+            if (_functional != functionalState::UNKNOWN)
+            {
+                log<level::INFO>("Setting a fan back to functional",
+                                 entry("FAN=%s", _name.c_str()));
+            }
+
+            //TODO: actually update inventory
+        }
+    }
 }
 
 
@@ -131,7 +172,7 @@ void Fan::timerExpired(TachSensor* sensor)
     //If the fan is currently functional, but too many
     //contained sensors are now nonfunctional, update
     //the whole fan nonfunctional.
-	//TODO
+    //TODO
 }
 
 }
