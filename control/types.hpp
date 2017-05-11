38bd1a9c3d58f5509d613cee6e15dfcc84d72424
Matthew Barth <msbarth@us.ibm.com>
2017-05-11 14:12:27 -0500

    Add property changed signal handler
    Enable the control application to handle property changed signals to set
    or update a cached set of these properties
    
    Change-Id: Ib84ffe1e801ee7dd85d17fdbb122d124d307dbd3
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/types.hpp
+++ control/types.hpp
@@ -10,6 +10,8 @@ namespace fan
 namespace control
 {
 
+class Zone;
+
 //Placeholder. Conditions are completely TBD.
 using Condition = bool;
 
@@ -17,12 +19,26 @@ constexpr auto fanNamePos = 0;
 constexpr auto sensorListPos = 1;
 using FanDefinition = std::tuple<std::string, std::vector<std::string>>;
 
+using Handler = std::function<void(sdbusplus::bus::bus&,
+                                   sdbusplus::message::message&,
+                                   Zone&)>;
+
+constexpr auto signaturePos = 0;
+constexpr auto handlerObjPos = 1;
+using PropertyChange = std::tuple<std::string, Handler>;
+using SetSpeedEvent = std::vector<PropertyChange>;
+
+constexpr auto zoneObjPos = 0;
+using SignalEvent = std::tuple<Zone*, Handler>;
+
 constexpr auto zoneNumPos = 0;
 constexpr auto fullSpeedPos = 1;
 constexpr auto fanListPos = 2;
+constexpr auto setSpeedEventsPos = 3;
 using ZoneDefinition = std::tuple<size_t,
                                   unsigned int,
-                                  std::vector<FanDefinition>>;
+                                  std::vector<FanDefinition>,
+                                  std::vector<SetSpeedEvent>>;
 
 constexpr auto conditionListPos = 0;
 constexpr auto zoneListPos = 1;
