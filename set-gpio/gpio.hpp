b2bca5fb20b869a05891fa67936e2469d290c2ff
Matt Spinler <spinler@us.ibm.com>
2017-05-24 12:21:21 -0500

    Add GPIO class
    This class is used for writing a GPIO via the
    /dev/gpiochipX interface.
    
    It requests a GPIO line handle from the GPIO device to
    do the actual operation on.
    
    The GPIO number to use, for AST chips at least, is the
    actual GPIO number, such as GPIOA0 = 0 and GPIOA7 = 7.
    
    Reading isn't currently supported because the kernel
    doesn't seem to always return correct values.
    
    Change-Id: I7802ead851af6de7fde802407958c7a1363a143f
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ set-gpio/gpio.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <memory>
+#include "utility.hpp"
+
+namespace phosphor
+{
+namespace gpio
+{
+
+
+/**
+ * Represents a GPIO.
+ *
+ * Operations are setting low or high.
+ *
+ * Plumbing for doing a read is present, but not the
+ * public API because it doesn't return reliable values.
+ */
+class GPIO
+{
+    public:
+
+        /**
+         * If the GPIO is an input or output
+         */
+        enum class Direction
+        {
+            input,
+            output
+        };
+
+        /**
+         * The possible values - low or high
+         */
+        enum class Value
+        {
+            low,
+            high
+        };
+
+        GPIO() = delete;
+        GPIO(const GPIO&) = delete;
+        GPIO(GPIO&&) = default;
+        GPIO& operator=(const GPIO&) = delete;
+        GPIO& operator=(GPIO&&) = default;
+        ~GPIO() = default;
+
+        /**
+         * Constructor
+         *
+         * @param[in] device - the GPIO device file
+         * @param[in] gpio - the GPIO number
+         * @param[in] direction - the GPIO direction
+         */
+        GPIO(const std::string& device,
+             unsigned int gpio,
+             Direction direction) :
+            _device(device),
+            _gpio(gpio),
+            _direction(direction)
+        {
+        }
+
+        /**
+         * Sets the GPIO high
+         */
+        inline void setHigh()
+        {
+            setGPIO(Value::high);
+        }
+
+        /**
+         * Sets the GPIO low
+         */
+        inline void setLow()
+        {
+            setGPIO(Value::low);
+        }
+
+    private:
+
+        /**
+         * Requests a GPIO line from the GPIO device
+         *
+         * @param[in] defaultValue - The default value, required for
+         *                           output GPIOs only.
+         */
+        void requestLine(Value defaultValue = Value::high);
+
+        /**
+         * Sets the GPIO to low or high
+         *
+         * Requests the GPIO line if it hasn't been done already.
+         */
+        void setGPIO(Value value);
+
+        /**
+         * The GPIO device name, like /dev/gpiochip0
+         */
+        const std::string _device;
+
+        /**
+         * The GPIO number
+         */
+        const unsigned int _gpio;
+
+        /**
+         * The GPIO direction
+         */
+        const Direction _direction;
+
+        /**
+         * File descriptor for the GPIO line
+         */
+        std::unique_ptr<phosphor::fan::util::FileDescriptor> _lineFD;
+};
+
+}
+}
