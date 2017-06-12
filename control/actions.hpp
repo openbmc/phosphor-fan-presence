d256b9fcbac7016acedcb5ceb2a91b9d576667cd
Matthew Barth <msbarth@us.ibm.com>
2017-06-12 13:39:31 -0500

    Allow floor speed changes based on sensor values
    Given a group of sensor values, the average of those sensor values will
    be used in selecting the floor speed for the zone. Each time the speed
    is set/updated, any speed requested lower than the current floor will be
    overwritten with the floor speed to not allow speeds below the floor.
    
    Change-Id: I4c8e8a2cd66892b9fdc2bc5643e907adddff51f8
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/actions.hpp
+++ control/actions.hpp
@@ -50,6 +50,57 @@ auto count_state_before_speed(size_t count, T&& state, uint64_t speed)
     };
 }
 
+/**
+ * @brief An action to set the floor speed on a zone
+ * @details Based on the average of the defined sensor group values, the floor
+ * speed is selected from the first map key entry that the average sensor value
+ * is less than.
+ *
+ * @param[in] val_to_speed - Ordered map of sensor value-to-speed
+ *
+ * @return Lambda function
+ *     A lambda function to set the zone's floor speed when the average of
+ *     property values within the group is below the lowest sensor value given
+ */
+auto set_floor_from_average_sensor_value(
+        std::map<int64_t, uint64_t>&& val_to_speed)
+{
+    return [val_to_speed = std::move(val_to_speed)](auto& zone, auto& group)
+    {
+        auto speed = zone.getDefFloor();
+        if (group.size() != 0)
+        {
+            auto sumTemp = 0;
+            std::for_each(
+                group.begin(),
+                group.end(),
+                [&zone, &sumTemp](auto const& entry)
+                {
+                    sumTemp +=
+                        zone.template getPropertyValue<int64_t>(
+                                entry.first,
+                                std::get<intfPos>(entry.second),
+                                std::get<propPos>(entry.second));
+                }
+            );
+            auto avgValue= sumTemp / group.size();
+            auto it = std::find_if(
+                val_to_speed.begin(),
+                val_to_speed.end(),
+                [&avgValue](auto const& entry)
+                {
+                    return avgValue < entry.first;
+                }
+            );
+            if (it != std::end(val_to_speed))
+            {
+                speed = (*it).second;
+            }
+        }
+        zone.setFloor(speed);
+    };
+}
+
 } // namespace action
 } // namespace control
 } // namespace fan
