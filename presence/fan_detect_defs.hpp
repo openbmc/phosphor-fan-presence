c040e4675bfbd689634bcee88bc4a1ae75e7a190
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-07-12 15:57:14 -0400

    presence: Switch to new framework
    Switch the main application logic to the output produced by
    the new parser previously.
    Remove unused code.
    
    Resolves: openbmc/openbmc#1318
    
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>
    Change-Id: If8b262bd80fd0f0135aeea55e578fb3c7f44339a

--- presence/fan_detect_defs.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <map>
-#include <set>
-#include <tuple>
-#include "fan_properties.hpp"
-
-extern const std::map<std::string,
-                      std::set<phosphor::fan::Properties>> fanDetectMap;
