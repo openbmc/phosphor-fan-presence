cec01830bc9718cb1da20a92856798ae8abbfc87
Matthew Barth <msbarth@us.ibm.com>
2017-09-26 09:15:56 -0500

    Update event initialization
    Using a null dbus message to the event signal will perform the necessary
    steps to initialize the event parameters using the given signal
    handler's function directly.
    
    i.e.) In the case for subscribing to PropertiesChanged signals,
    initially the given property must be read. When the PropertiesChanged
    signal struct is given a null message, it finds the property, reads the
    value, and then can perform the given signal handler function directly.
    This produces the same functional path as receiving a PropertiesChanged
    signal containing the property value within the message.
    
    Change-Id: I35575cfff66eb0305156be786cb1f5536d42bb1c
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ control/matches.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include "sdbusplus.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+namespace match
+{
+
+using namespace phosphor::fan;
+using namespace sdbusplus::bus::match;
+
+/**
+ * @brief A match function that constructs a PropertiesChanged match string
+ * @details Constructs a PropertiesChanged match string with a given object
+ * path and interface
+ *
+ * @param[in] obj - Object's path name
+ * @param[in] iface - Interface name
+ *
+ * @return - A PropertiesChanged match string
+ */
+inline auto propertiesChanged(const std::string& obj, const std::string& iface)
+{
+    return rules::propertiesChanged(obj, iface);
+}
+
+/**
+ * @brief A match function that constructs an InterfacesAdded match string
+ * @details Constructs an InterfacesAdded match string with a given object
+ * path
+ *
+ * @param[in] obj - Object's path name
+ *
+ * @return - A InterfacesAdded match string
+ */
+inline auto interfacesAdded(const std::string& obj)
+{
+    return rules::interfacesAdded(obj);
+}
+
+/**
+ * @brief A match function that constructs a NameOwnerChanged match string
+ * @details Constructs a NameOwnerChanged match string with a given object
+ * path and interface
+ *
+ * @param[in] obj - Object's path name
+ * @param[in] iface - Interface name
+ *
+ * @return - A NameOwnerChanged match string
+ */
+inline auto nameOwnerChanged(const std::string& obj, const std::string& iface)
+{
+    std::string noc;
+    try
+    {
+        noc = rules::nameOwnerChanged() +
+                rules::argN(0, util::SDBusPlus::getService(obj, iface));
+    }
+    catch (const InternalFailure& ife)
+    {
+        // Unable to construct NameOwnerChanged match string
+    }
+    return noc;
+}
+
+} // namespace match
+} // namespace control
+} // namespace fan
+} // namespace phosphor
