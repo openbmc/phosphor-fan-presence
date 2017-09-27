67f37a976ae1d797ceb65f20e93c1b51fb610332
Matthew Barth <msbarth@us.ibm.com>
2017-09-27 09:33:42 -0500

    Set/update a service name owner for a group
    Adds a service name associated with a group when the group or given
    service name is not found within the map of services otherwise updates
    the service name owner associated with the group.
    
    Change-Id: I602ddaa7e06e354e01ea70d6c5c0a45e74a14e99
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/zone.cpp
+++ control/zone.cpp
@@ -115,6 +115,36 @@ void Zone::setActiveAllow(const Group* group, bool isActiveAllow)
     }
 }
 
+void Zone::setServiceOwner(const Group* group,
+                           const std::string& name,
+                           const std::string& owner)
+{
+    try
+    {
+        auto& sNames = _services.at(*(group));
+        auto it = std::find_if(
+            sNames.begin(),
+            sNames.end(),
+            [&name](auto const& entry)
+            {
+                return name == std::get<namePos>(entry);
+            }
+        );
+        if (it != std::end(sNames))
+        {
+            std::get<ownerPos>((*it)) = owner;
+        }
+        else
+        {
+            _services[*(group)].push_back({name, owner});
+        }
+    }
+    catch (const std::out_of_range& oore)
+    {
+        _services[*(group)].push_back({name, owner});
+    }
+}
+
 void Zone::setFloor(uint64_t speed)
 {
     _floorSpeed = speed;
