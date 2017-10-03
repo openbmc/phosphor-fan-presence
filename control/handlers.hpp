142eefc41349fec106ad6b31ccd89f96d78f6712
Matthew Barth <msbarth@us.ibm.com>
2017-10-03 11:27:06 -0500

    Fill in NameOwnerChanged signal support
    A NameOwnerChanged signal message provides three strings containing the
    service name that has changed along with its old service owner name and
    new service owner name. These names are then passed along to the handler
    to find/update the group associated with the changed owner name.
    
    Change-Id: I7d67883b010fec5b282bd00a4dcc29629486af00
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/handlers.hpp
+++ control/handlers.hpp
@@ -44,7 +44,8 @@ auto setService(Group&& group)
 {
     return [group = std::move(group)](auto& zone, auto& name, bool hasOwner)
     {
-        // TODO Update service name owner state list of a group
+        // Update service name owner state list of a group
+        zone.setServiceOwner(&group, name, hasOwner);
     };
 }
 
