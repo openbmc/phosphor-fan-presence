4de028979a71ef56a59157292922df2e740638fc
Matthew Barth <msbarth@us.ibm.com>
2017-10-05 17:03:40 -0500

    Add timer event management
    Events defined to have timers require the ability to find a timer, add a
    timer to the event, and removing a timer event entirely. These event
    timers are intended to allow actions based on the event to be delayed or
    recurring based on the timer defined.
    
    Change-Id: Ieaf26f031c5e5aac9472e92354bfb76392642cb4
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/types.hpp
+++ control/types.hpp
@@ -81,8 +81,11 @@ constexpr auto eventHandlerPos = 2;
 constexpr auto eventActionsPos = 3;
 using EventData = std::tuple<Group, std::string, Handler, std::vector<Action>>;
 
-constexpr auto timerTimerPos = 0;
-using TimerEvent = std::tuple<phosphor::fan::util::Timer>;
+constexpr auto timerEventDataPos = 0;
+constexpr auto timerTimerPos = 1;
+using TimerEvent =
+    std::tuple<std::unique_ptr<EventData>,
+               std::unique_ptr<phosphor::fan::util::Timer>>;
 
 constexpr auto signalEventDataPos = 0;
 constexpr auto signalMatchPos = 1;
