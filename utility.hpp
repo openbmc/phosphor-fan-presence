e0c3e51740c432cd9a5553bc6635dc513b064a5b
Matthew Barth <msbarth@us.ibm.com>
2017-11-16 15:21:13 -0600

    Update fan inventory method
    Use the methods provided within sdbusplus.hpp to update fan inventory
    along with prepping inventory to be updated for each fan rotor sensor's
    functional state.
    
    Change-Id: I7d3026d289b1dd22cd4e7b4457c4d4396309c0b5
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- utility.hpp
+++ utility.hpp
@@ -20,6 +20,17 @@ namespace fan
 namespace util
 {
 
+constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
+
+constexpr auto INVENTORY_PATH = "/xyz/openbmc_project/inventory";
+constexpr auto INVENTORY_INTF = "xyz.openbmc_project.Inventory.Manager";
+
+constexpr auto OPERATIONAL_STATUS_INTF =
+    "xyz.openbmc_project.State.Decorator.OperationalStatus";
+constexpr auto FUNCTIONAL_PROPERTY = "Functional";
+
 class FileDescriptor
 {
     public:
@@ -89,6 +100,43 @@ std::string getInvService(sdbusplus::bus::bus& bus);
 std::string getService(const std::string& path,
                        const std::string& interface,
                        sdbusplus::bus::bus& bus);
+
+/**
+ * @brief Get the object map for creating or updating an object property
+ *
+ * @param[in] path - The dbus object path name
+ * @param[in] intf - The dbus interface
+ * @param[in] prop - The dbus property
+ * @param[in] value - The property value
+ *
+ * @return - The full object path containing the property value
+ */
+template <typename T>
+auto getObjMap(const std::string& path,
+               const std::string& intf,
+               const std::string& prop,
+               const T& value)
+{
+    using Property = std::string;
+    using Value = sdbusplus::message::variant<T>;
+    using PropertyMap = std::map<Property, Value>;
+
+    using Interface = std::string;
+    using InterfaceMap = std::map<Interface, PropertyMap>;
+
+    using Object = sdbusplus::message::object_path;
+    using ObjectMap = std::map<Object, InterfaceMap>;
+
+    ObjectMap objectMap;
+    InterfaceMap interfaceMap;
+    PropertyMap propertyMap;
+
+    propertyMap.emplace(prop, value);
+    interfaceMap.emplace(intf, std::move(propertyMap));
+    objectMap.emplace(path, std::move(interfaceMap));
+
+    return objectMap;
+}
 
 }
 }
