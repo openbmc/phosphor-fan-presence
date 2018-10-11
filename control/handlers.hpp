ece7295beae4aff9c86fdcec9ed8222fa8139a20
Matthew Barth <msbarth@us.ibm.com>
2018-10-11 14:10:47 -0500

    Optimize event group property handling
    Use the event group when handling signals and allow a group subset to be
    given on the event for retrieving group member properties.
    
    Change-Id: I5d4ae072fa540a02f22c0d85cdc03f3686a6816d
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/handlers.hpp
+++ control/handlers.hpp
@@ -12,7 +12,7 @@ namespace handler
 /**
  * @brief A handler function to set/update a property
  * @details Sets or updates a property's value determined by a combination of
- * an object's path and property names
+ * an object's path, interface, and property names
  *
  * @param[in] path - Object's path name
  * @param[in] interface - Object's interface name
@@ -22,11 +22,11 @@ namespace handler
  *     A lambda function to set/update the property value
  */
 template <typename T>
-auto setProperty(const char* path, const char* interface, const char* property)
+auto setProperty()
 {
-    return [=](auto& zone, T&& arg)
+    return [](auto& zone, auto& path, auto& intf, auto& prop, T&& arg)
     {
-        zone.setPropertyValue(path, interface, property, std::forward<T>(arg));
+        zone.setPropertyValue(path, intf, prop, std::forward<T>(arg));
     };
 }
 
