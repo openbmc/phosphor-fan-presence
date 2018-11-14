549eb91dbc0ae6f1fd3e38a887efe65b8048c671
Matthew Barth <msbarth@us.ibm.com>
2018-11-14 15:03:05 -0600

    Provide event name to triggers
    Change-Id: Iec063b39a3e3e6a63038a2013f42e8a69ad9fadf
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/zone.cpp
+++ control/zone.cpp
@@ -311,6 +311,7 @@ void Zone::initEvent(const SetSpeedEvent& event)
         [this, &event](auto const& trigger)
         {
             trigger(*this,
+                    std::get<sseNamePos>(event),
                     std::get<groupPos>(event),
                     std::get<actionsPos>(event));
         }
