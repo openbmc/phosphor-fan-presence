eb9184cd428b3b4a3892c3d43ff18637fdfc1032
Matthew Barth <msbarth@us.ibm.com>
2018-11-20 09:54:30 -0600

    Generate groups defined on actions
    Change-Id: Iac5b31f01cb402d45abef1bf14df1405cafd3dbc
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/templates/fan_zone_defs.mako.cpp
+++ control/templates/fan_zone_defs.mako.cpp
@@ -100,17 +100,17 @@ const std::vector<ZoneGroup> Manager::_zoneLayouts
                         (event['pc'] is not None):
                     SetSpeedEvent{
                         "${event['pc']['pcname']}",
-                        Group{
+                        Group
+                        {
                         %for group in event['pc']['pcgrps']:
                         %for member in group['members']:
-                        {
-                            "${member['object']}",
+                            {"${member['object']}",
                             "${member['interface']}",
-                            "${member['property']}"
-                        },
+                            "${member['property']}"},
                         %endfor
                         %endfor
                         },
+                        ActionData{
                         {Group{},
                         std::vector<Action>{
                         %for i, a in enumerate(event['pc']['pcact']):
@@ -160,6 +160,7 @@ const std::vector<ZoneGroup> Manager::_zoneLayouts
                         ),
                         %endif
                         }},
+                        },
                         std::vector<Trigger>{
                             %if ('timer' in event['pc']['triggers']) and \
                                 (event['pc']['triggers']['timer'] is not None):
