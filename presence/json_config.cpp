8be5833f00f5f9b8225fbac915ab727f72ba1bb1
Matthew Barth <msbarth@us.ibm.com>
2019-11-14 15:01:57 -0600

    presence: Add json config class
    Add a json config class that will be used to parse a given json config
    file and generate the fan presence policies from that config.
    
    Tested:
        N/A
    
    Change-Id: Iac1703ea8d9e1e1d0d40e15a58145181d4f1a0f9
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ presence/json_config.cpp
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <string>
+
+#include "json_config.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace presence
+{
+
+policies JsonConfig::_policies;
+
+JsonConfig::JsonConfig(const std::string& jsonFile)
+{
+
+}
+
+const policies& JsonConfig::getPolicies()
+{
+    return _policies;
+}
+
+} // namespace presence
+} // namespace fan
+} // namespace phosphor
