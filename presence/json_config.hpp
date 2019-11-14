fe58831f63194a0ba1e859a44a1f67837bbd38cf
Matthew Barth <msbarth@us.ibm.com>
2019-11-14 15:01:57 -0600

    presence: Add json config class
    Add a json config class that will be used to parse a given json config
    file and generate the fan presence policies from that config.
    
    Tested:
        N/A
    
    Change-Id: Iac1703ea8d9e1e1d0d40e15a58145181d4f1a0f9
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ presence/json_config.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "config.h"
+#include "rpolicy.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace presence
+{
+
+using policies = std::vector<std::unique_ptr<RedundancyPolicy>>;
+
+class JsonConfig
+{
+    public:
+
+        JsonConfig() = delete;
+        JsonConfig(const JsonConfig&) = delete;
+        JsonConfig(JsonConfig&&) = delete;
+        JsonConfig& operator=(const JsonConfig&) = delete;
+        JsonConfig& operator=(JsonConfig&&) = delete;
+        ~JsonConfig() = default;
+
+        /**
+         * Constructor
+         * Parses and populates the fan presence policies from a json file
+         *
+         * @param[in] jsonFile - json configuration file
+         */
+        explicit JsonConfig(const std::string& jsonFile);
+
+        /**
+         * @brief Get the json config based fan presence policies
+         *
+         * @return - The fan presence policies
+         */
+        static const policies& get();
+
+    private:
+
+        /* Fan presence policies */
+        static policies _policies;
+};
+
+} // namespace presence
+} // namespace fan
+} // namespace phosphor
