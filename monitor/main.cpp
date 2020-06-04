ccaf2db92c46104c13c207d9ddd4a34a359de892
Matthew Barth <msbarth@us.ibm.com>
2020-06-04 13:09:35 -0500

    monitor:JSON: Find and load JSON configuration
    Using the JSON config utility to find and load the appropriate JSON
    configuration file, the JSON parsed object is returned. This object will
    be what's used to parse each section of fan monitor's configuration.
    
    Tested:
        JSON configuration file found and loaded at each possible location
    
    Change-Id: I718ad3daa240d654ecee3733ebca2d7743ba9d17
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -13,8 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "config.h"
+
 #include "argument.hpp"
 #include "fan.hpp"
+#ifdef MONITOR_USE_JSON
+#include "json_parser.hpp"
+#endif
 #include "fan_defs.hpp"
 #include "trust_manager.hpp"
 
@@ -53,12 +59,15 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    std::unique_ptr<phosphor::fan::trust::Manager> trust =
-        std::make_unique<phosphor::fan::trust::Manager>(trustGroups);
-
     // Attach the event object to the bus object so we can
     // handle both sd_events (for the timers) and dbus signals.
     bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+#ifdef MONITOR_USE_JSON
+    // Get JSON object from monitor JSON config file
+    const auto& jsonObj = getJsonObj(bus);
+#else
+    auto trust = std::make_unique<phosphor::fan::trust::Manager>(trustGroups);
 
     for (const auto& fanDef : fanDefinitions)
     {
@@ -75,6 +84,7 @@ int main(int argc, char* argv[])
         fans.emplace_back(
             std::make_unique<Fan>(mode, bus, event, trust, fanDef));
     }
+#endif
 
     if (mode == Mode::init)
     {
