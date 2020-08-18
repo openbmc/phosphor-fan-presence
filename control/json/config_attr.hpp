618427434c049222e0f30939a52f6a7a874305dc
Matthew Barth <msbarth@us.ibm.com>
2020-08-18 14:48:38 -0500

    control: Base JSON configuration attribute class
    Base class to be extended by each JSON configuration file parsing
    classes. All JSON configuration attributes must have a name provided.
    This name will be used to index into a map of each configuration
    attribute type to retrieve its corresponding configuration object.
    
    Tested:
        Name is retrieved from JSON object provided
        Throws runtime exception when "name" is not found
    
    Change-Id: I4a19887fffa05ca582422277469de9ec9b06fe92
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ control/json/config_attr.hpp
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/log.hpp>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+using namespace phosphor::logging;
+
+/**
+ * @class ConfigAttr - Configuration attribute
+ *
+ * Base class for fan control's JSON configuration attributes.
+ */
+class ConfigAttr
+{
+  public:
+    ConfigAttr() = delete;
+    ConfigAttr(const ConfigAttr&) = delete;
+    ConfigAttr(ConfigAttr&&) = delete;
+    ConfigAttr& operator=(const ConfigAttr&) = delete;
+    ConfigAttr& operator=(ConfigAttr&&) = delete;
+    virtual ~ConfigAttr() = default;
+
+    explicit ConfigAttr(const json& jsonObj)
+    {
+        // Set the name of this profile
+        setName(jsonObj);
+    }
+
+    /**
+     * @brief Get the profile's name
+     *
+     * @return Name of the profile
+     */
+    inline const std::string& getName()
+    {
+        return _name;
+    }
+
+  private:
+    /* Name of the profile */
+    std::string _name;
+
+    /**
+     * @brief Sets the profile name from the given JSON
+     *
+     * @param[in] jsonObj - JSON to get profile name from
+     */
+    inline void setName(const json& jsonObj)
+    {
+        if (!jsonObj.contains("name"))
+        {
+            // Log error on missing profile name
+            log<level::ERR>("Missing required profile name",
+                            entry("JSON=%s", jsonObj.dump().c_str()));
+            throw std::runtime_error("Missing required profile name");
+        }
+        _name = jsonObj["name"].get<std::string>();
+    }
+};
+
+} // namespace phosphor::fan::control::json
