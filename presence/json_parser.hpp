901c98f3ab65f28e255ceca8cb4e05e1939d4108
Jolie Ku <jolie_ku@wistron.com>
2020-08-24 16:32:15 +0800

    presence: Use shared JSON config file finding & loading header
    Use the shared JSON config determination header for loading its
    JSON configuration file.
    
    Tested:
        run phosphor-fan-presence-tach can find and load config.json from
        /usr/share/phosphor-fan-presence/presence and
        /etc/phosphor-fan-presence/presence/ in witherspoon qemu
    
    Change-Id: I5c4fdf7d96de7c956ca24b4a5751911cc1bf1f03
    Signed-off-by: Jolie Ku <jolie_ku@wistron.com>

--- presence/json_parser.hpp
+++ presence/json_parser.hpp
@@ -24,12 +24,7 @@ namespace fs = std::filesystem;
 using json = nlohmann::json;
 
 constexpr auto confFileName = "config.json";
-constexpr auto confOverridePath = "/etc/phosphor-fan-presence/presence";
-constexpr auto confBasePath = "/usr/share/phosphor-fan-presence/presence";
-constexpr auto confDbusPath = "/xyz/openbmc_project/inventory/system/chassis";
-constexpr auto confDbusIntf =
-    "xyz.openbmc_project.Inventory.Decorator.Compatible";
-constexpr auto confDbusProp = "Names";
+constexpr auto confAppName = "presence";
 
 using policies = std::vector<std::unique_ptr<RedundancyPolicy>>;
 
@@ -86,9 +81,6 @@ class JsonConfig
     /* The sdbusplus bus object */
     sdbusplus::bus::bus& _bus;
 
-    /* Config file to be used */
-    fs::path _confFile;
-
     /* List of Fan objects to have presence policies */
     std::vector<fanPolicy> _fans;
 
@@ -100,24 +92,6 @@ class JsonConfig
      * function
      */
     static const std::map<std::string, rpolicyHandler> _rpolicies;
-
-    /**
-     * Get the json configuration file. The first location found to contain
-     * the json config file is used from the following locations in order.
-     * 1.) From the confOverridePath location
-     * 2.) From config file found using property value(s) as a relative
-     * path extension on the base path from the dbus object where:
-     *     path = Path set in confDbusPath
-     *     interface = Interface set in confDbusIntf
-     *     property = Property set in confDbusProp
-     * 3.) *DEFAULT* - From the confBasePath location
-     */
-    const fs::path getConfFile();
-
-    /**
-     * @brief Load the json config file
-     */
-    void load();
 
     /**
      * @brief Process the json config to extract the defined fan presence
