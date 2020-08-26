447319ed2e174a6886ad12e418b5f314bf51a1d9
Matt Spinler <spinler@us.ibm.com>
2020-08-26 15:26:26 -0500

    WIP: Add a fan unit test
    But unless WANT_JSON is set in CI, these won't run in CI.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: Iabdac95c3cca8239fa2cddc495e47f0d0ae44d04

---
+++ control/json/test/profile_test.cpp
@@ -0,0 +1,37 @@
+#include "control/json/profile.hpp"
+
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::fan::control::json;
+
+TEST(ProfileTest, Test)
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    {
+        static const auto json = R"(
+        {
+            "name": "Test",
+            "method": {
+                "name": "all_of",
+                "properties": []
+            }
+        }
+        )"_json;
+
+        Profile profile{bus, json};
+    }
+
+    {
+        static const auto json = R"(
+        {
+            "name": "Test"
+        }
+        )"_json;
+
+        EXPECT_THROW(Profile profile(bus, json), std::runtime_error);
+    }
+}
