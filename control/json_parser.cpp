8b384deb90202a408b88eac32f9c7054d2d278b7
Matthew Barth <msbarth@us.ibm.com>
2020-09-29 10:44:37 -0500

    control: Construct base zone group from JSON
    Pulls together the profiles, zones, and fans JSON configurations to
    construct a single zone group to be used as fan control's configuration.
    This base zone group includes all that is necessary to get a basic fan
    control configuration that will hold fans at the zone's given full
    speed. A single zone group is created since the active state of any
    configured profiles is checked while creating the zone group.
    
    *Note that "profiles" in JSON configs replace what was called
    "Conditions" in YAML based configs.
    
    Tested:
        Loaded profile based configurations
        Loaded non-profile based configurations
        Loaded combination of profile and non-profile based configurations
    
    Change-Id: Id010c899a7633824b80c5cef21c848eadfb66243
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/json_parser.cpp
+++ control/json_parser.cpp
@@ -25,8 +25,38 @@
 
 #include <sdbusplus/bus.hpp>
 
+#include <algorithm>
+#include <cstdlib>
+#include <tuple>
+#include <vector>
+
 namespace phosphor::fan::control
 {
+
+const bool checkEntry(const std::vector<std::string>& activeProfiles,
+                      const std::vector<std::string>& entryProfiles)
+{
+    // Include entry if its list of profiles to be included in is empty
+    if (entryProfiles.empty())
+    {
+        // Entry always to be included
+        return true;
+    }
+    else
+    {
+        for (const auto& profile : activeProfiles)
+        {
+            auto iter =
+                std::find(entryProfiles.begin(), entryProfiles.end(), profile);
+            if (iter != entryProfiles.end())
+            {
+                // Entry configured to be included in active profile
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 const std::vector<ZoneGroup> getZoneGroups(sdbusplus::bus::bus& bus)
 {
@@ -46,7 +76,71 @@ const std::vector<ZoneGroup> getZoneGroups(sdbusplus::bus::bus& bus)
         auto groups = getConfig<json::Group>(bus);
     }
 
-    // TODO Create zone groups after loading all JSON config files
+    // Ensure all configurations use the same set of active profiles
+    // (In case a profile's active state changes during configuration)
+    std::vector<std::string> activeProfiles;
+    for (const auto& profile : profiles)
+    {
+        if (profile.second->isActive())
+        {
+            activeProfiles.emplace_back(profile.first.first);
+        }
+    }
+
+    // Conditions list empty for JSON based configurations
+    // TODO Remove conditions after YAML based configuration removed
+    std::vector<Condition> conditions;
+    std::vector<ZoneDefinition> zoneDefs;
+    for (const auto& zone : zones)
+    {
+        // Check zone profiles against active profiles
+        if (checkEntry(activeProfiles, zone.second->getProfiles()))
+        {
+            auto zoneName = zone.second->getName();
+            // Create FanDefinition list for zone
+            std::vector<FanDefinition> fanDefs;
+            for (const auto& fan : fans)
+            {
+                // Check fan profiles against active profiles and
+                // if the fan is included in the zone
+                if (checkEntry(activeProfiles, fan.second->getProfiles()) &&
+                    fan.second->getZone() == zoneName)
+                {
+                    fanDefs.emplace_back(std::make_tuple(
+                        fan.second->getName(), fan.second->getSensors(),
+                        fan.second->getInterface()));
+                }
+            }
+
+            // Create SetSpeedEvents list for zone
+            std::vector<SetSpeedEvent> speedEvents;
+            // TODO Populate SetSpeedEvents list with configured events
+
+            // YAML zone name was an integer, JSON is a string
+            // Design direction is for it to be a string and this can be
+            // removed after YAML based configurations are removed.
+            char* zoneName_end;
+            auto zoneNum = std::strtol(zoneName.c_str(), &zoneName_end, 10);
+            if (*zoneName_end)
+            {
+                throw std::runtime_error(
+                    "Zone names must be a string representation of a number");
+            }
+            else
+            {
+                zoneDefs.emplace_back(std::make_tuple(
+                    zoneNum, zone.second->getFullSpeed(),
+                    zone.second->getDefaultFloor(), zone.second->getIncDelay(),
+                    zone.second->getDecInterval(),
+                    zone.second->getZoneHandlers(), std::move(fanDefs),
+                    std::move(speedEvents)));
+            }
+        }
+    }
+    // TODO Should only result in a single entry but YAML based configurations
+    // produce a list of zone groups. Change to single zone group after YAML
+    // based configuartions are removed.
+    zoneGrps.emplace_back(std::make_tuple(conditions, zoneDefs));
 
     return zoneGrps;
 }
