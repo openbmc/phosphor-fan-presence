41071c737f34a5349c9af3ccf61cbf1437df5e36
Matthew Barth <msbarth@us.ibm.com>
2021-01-29 09:04:43 -0600

    control: Add request speed base action
    Add the YAML based set_request_speed_base_with_max action function as an
    action class for JSON configs to use. This action required to be
    enhanced to handle the different supported data types of groups that
    could be configured in the JSON with this action.
    
    Change-Id: I8cc223cc10d33462ddc303145fee08347ea7c8b5
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ control/json/actions/request_speed_base.cpp
@@ -0,0 +1,94 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "request_speed_base.hpp"
+
+#include "types.hpp"
+#include "zone.hpp"
+
+#include <fmt/format.h>
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/log.hpp>
+
+#include <algorithm>
+#include <tuple>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+using namespace phosphor::logging;
+
+RequestSpeedBase::RequestSpeedBase(const json&) :
+    ActionBase(RequestSpeedBase::name)
+{
+    // There are no JSON configuration parameters for this action
+}
+
+void RequestSpeedBase::run(Zone& zone, const Group& group)
+{
+    uint64_t base = 0;
+    for (const auto& member : group)
+    {
+        try
+        {
+            auto value = zone.getPropValueVariant(std::get<pathPos>(member),
+                                                  std::get<intfPos>(member),
+                                                  std::get<propPos>(member));
+            if (auto intPtr = std::get_if<int64_t>(&value))
+            {
+                // Throw out any negative values as those are not valid
+                // to use as a fan speed base
+                if (*intPtr < 0)
+                {
+                    continue;
+                }
+                base = std::max(base, static_cast<uint64_t>(*intPtr));
+            }
+            else if (auto dblPtr = std::get_if<double>(&value))
+            {
+                // Throw out any negative values as those are not valid
+                // to use as a fan speed base
+                if (*dblPtr < 0)
+                {
+                    continue;
+                }
+                // Precision of a double not a concern with fan speeds
+                base = std::max(base, static_cast<uint64_t>(*dblPtr));
+            }
+            else
+            {
+                // Unsupported group member type for this action
+                log<level::ERR>(
+                    fmt::format("Action {}: Unsupported group member type "
+                                "given. [object = {} : {} : {}]",
+                                getName(), std::get<pathPos>(member),
+                                std::get<intfPos>(member),
+                                std::get<propPos>(member))
+                        .c_str());
+            }
+        }
+        catch (const std::out_of_range& oore)
+        {
+            // Property value not found, base request speed unchanged
+        }
+    }
+
+    // A request speed base of 0 defaults to the current target speed
+    zone.setRequestSpeedBase(base);
+}
+
+} // namespace phosphor::fan::control::json
---
+++ control/json/actions/request_speed_base.hpp
@@ -0,0 +1,75 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "action.hpp"
+#include "types.hpp"
+#include "zone.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+
+/**
+ * @class RequestSpeedBase - Action to set the requested speed base
+ *
+ * Sets the base of a calculated requested speed to the maximum value found
+ * from the properties given within a group. The requested speed base is what
+ * the calculated requested speed should be determined from when changing fan
+ * speeds. By default, the base of the next calculated requested speed is the
+ * current target speed of the zone. This action allows that base to be changed
+ * according to the maximum value found from a given group of dbus objects.
+ */
+class RequestSpeedBase :
+    public ActionBase,
+    public ActionRegister<RequestSpeedBase>
+{
+  public:
+    /* Name of this action */
+    static constexpr auto name = "set_request_speed_base_with_max";
+
+    RequestSpeedBase() = delete;
+    RequestSpeedBase(const RequestSpeedBase&) = delete;
+    RequestSpeedBase(RequestSpeedBase&&) = delete;
+    RequestSpeedBase& operator=(const RequestSpeedBase&) = delete;
+    RequestSpeedBase& operator=(RequestSpeedBase&&) = delete;
+    ~RequestSpeedBase() = default;
+
+    /**
+     * @brief Update the requested speed base
+     *
+     * No JSON configuration parameters required
+     */
+    explicit RequestSpeedBase(const json&);
+
+    /**
+     * @brief Run the action
+     *
+     * Determines the maximum value from the properties of the group of dbus
+     * objects and sets the requested speed base to this value. Only positive
+     * integer or floating point types are supported as these are the only
+     * valid types for a fan speed to be based off of.
+     *
+     * @param[in] zone - Zone to run the action on
+     * @param[in] group - Group of dbus objects the action runs against
+     */
+    void run(Zone& zone, const Group& group) override;
+};
+
+} // namespace phosphor::fan::control::json
