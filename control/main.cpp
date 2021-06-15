27958bf89252ed8c5c6497ec2b7296917b566b74
Matthew Barth <msbarth@us.ibm.com>
2021-06-15 11:32:03 -0500

    control: Temporarily handle starting at poweron
    For JSON based fan control the fan control init service is used to start
    fan control at poweron. The associated recipe needs to be updated where
    JSON based fan control only uses the non-init service and have it start
    at BMC standby. Once that is merged, this commit can be reverted.
    
    Change-Id: I893b8b8f2f73ef5ef842a3b7733c24c426df00e6
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- control/main.cpp
+++ control/main.cpp
@@ -83,6 +83,9 @@ int main(int argc, char* argv[])
                       std::placeholders::_1, std::placeholders::_2));
 
         phosphor::fan::util::SDBusPlus::getBus().request_name(CONTROL_BUSNAME);
+
+        // TODO - Set power state to on until fan control services updated
+        manager.powerStateChanged(true);
 #else
         Manager manager(phosphor::fan::util::SDBusPlus::getBus(), event, mode);
 
