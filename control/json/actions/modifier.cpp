95d45e09beca90f35715cb5061d1ad0c44ab73bf
Matt Spinler <spinler@us.ibm.com>
2021-07-26 12:40:05 -0600

    control: Use a group for net increase speed state
    The 'set_net_increase_speed' action currently uses a state value read
    from the JSON config to check the group values against to see if the fan
    speed should be increased.
    
    This commit adds the ability to specify a group (so a D-Bus property) to
    use for that state value instead of reading it from the JSON.  In
    addition, it allows this new state value to be modified by some
    mathematical expression before being used.  This modifier is implemented
    by a new Modifier class that reads its config out of the JSON.  It is
    optional and nothing will be modified is there is no 'modifier' section
    in the JSON.
    
    For example, if the JSON config for the 'set_net_increase_speed' action
    is:
    
    {
      "name": "set_net_increase_speed",
      "delta": 300,
      "state_source": {
        "group": "throttle_temp_proc_0",
        "modifier": {
          "expression": "subtract",
          "value": 3
        }
      }
    }
    
    Then the code will read the value of the property specified in the
    throttle_temp_proc_0 group, subtract 3 from it, and then use the result
    as the state value to run the action against.
    
    The only allowed modification expression at this point is 'subtract'.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: Ib80fa5a06b15473a3ea78f3a172d090a8e9027d1

---
+++ control/json/actions/modifier.cpp
@@ -0,0 +1,145 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "modifier.hpp"
+
+#include "json/manager.hpp"
+
+#include <fmt/format.h>
+
+#include <phosphor-logging/log.hpp>
+
+using namespace phosphor::logging;
+
+namespace phosphor::fan::control::json
+{
+
+const std::map<std::string, Modifier::Expression> Modifier::_expressions{
+    {"subtract", Expression::subtract}};
+
+Modifier::Modifier(const json& jsonObj)
+{
+    setValue(jsonObj);
+    setExpression(jsonObj);
+}
+
+void Modifier::setValue(const json& jsonObj)
+{
+    if (!jsonObj.contains("value"))
+    {
+        log<level::ERR>("modifier entry in JSON missing 'value'");
+        throw std::invalid_argument("invalid modifier JSON");
+    }
+
+    const auto& object = jsonObj.at("value");
+    if (auto boolPtr = object.get_ptr<const bool*>())
+    {
+        _value = *boolPtr;
+    }
+    else if (auto intPtr = object.get_ptr<const int64_t*>())
+    {
+        _value = *intPtr;
+    }
+    else if (auto doublePtr = object.get_ptr<const double*>())
+    {
+        _value = *doublePtr;
+    }
+    else if (auto stringPtr = object.get_ptr<const std::string*>())
+    {
+        _value = *stringPtr;
+    }
+    else
+    {
+        throw std::invalid_argument("invalid modifier JSON");
+    }
+}
+
+void Modifier::setExpression(const json& jsonObj)
+{
+    if (!jsonObj.contains("expression"))
+    {
+        log<level::ERR>("modifier entry in JSON missing 'expression'");
+        throw std::invalid_argument("invalid modifier JSON");
+    }
+
+    auto expression = jsonObj["expression"].get<std::string>();
+
+    auto e = _expressions.find(expression);
+    if (e == _expressions.end())
+    {
+        log<level::ERR>(
+            fmt::format("math {} in modifier JSON is invalid", expression)
+                .c_str());
+        throw std::invalid_argument("invalid modifier JSON");
+    }
+
+    _expression = e->second;
+}
+
+PropertyVariantType Modifier::doOp(const PropertyVariantType& value)
+{
+    // Just one op so far
+    return subtract(value);
+}
+
+/**
+ * @brief Variant visitor to return a value of the template type specified.
+ */
+template <typename T>
+struct ToTypeVisitor
+{
+    template <typename U>
+    T operator()(const U& t) const
+    {
+        if constexpr (std::is_arithmetic_v<U> && std::is_arithmetic_v<T>)
+        {
+            return static_cast<T>(t);
+        }
+        throw std::invalid_argument("Non arithmetic type used in ToTypeVisitor");
+    }
+};
+
+PropertyVariantType Modifier::subtract(const PropertyVariantType& val)
+{
+    PropertyVariantType result;
+
+    if (std::holds_alternative<double>(val))
+    {
+        result =
+            std::get<double>(val) - std::visit(ToTypeVisitor<double>(), _value);
+    }
+    else if (std::holds_alternative<int32_t>(val))
+    {
+        result = std::get<int32_t>(val) -
+                 std::visit(ToTypeVisitor<int32_t>(), _value);
+    }
+    else if (std::holds_alternative<int64_t>(val))
+    {
+        result = std::get<int64_t>(val) -
+                 std::visit(ToTypeVisitor<int64_t>(), _value);
+    }
+    else
+    {
+        log<level::ERR>(
+            "Unsupported data type for modifier group member property value");
+        throw std::invalid_argument{
+            "Unsupported data type for modifier group member property value"};
+    }
+
+    return result;
+}
+
+} // namespace phosphor::fan::control::json
