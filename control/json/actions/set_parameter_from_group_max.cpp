81bc880df19cabb5e4f0a7d625ebe9c1256f0558
Matt Spinler <spinler@us.ibm.com>
2021-08-06 09:25:45 -0500

    control: set_parameter_from_group_max action
    This action will write a D-Bus property value to the Manager's parameter
    cache.  The property value written is the maximum value of all the
    configured groups. The value can be modified using the Modifier utility
    class's expressions before storing it.
    
    The use case for this is that this action would be configured to write a
    parameter that could then be used by another action.
    
    An example JSON config is:
    
      {
        "name": "set_parameter_from_group",
        "parameter_name": "proc_0_throttle_temp",
        "modifier": {
          "operator": "minus",
          "value": 4
        }
      }
    
    This would read the maximum D-Bus property found in all the groups
    configured for the action, subtract 4 from it, and then store it as a
    Manager parameter under the name proc_0_throttle_temp.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I75d91110023e0de9908d694997676e4854917ea8

---
+++ control/json/actions/set_parameter_from_group_max.cpp
@@ -0,0 +1,144 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "set_parameter_from_group_max.hpp"
+
+#include "../manager.hpp"
+
+#include <fmt/format.h>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+using namespace phosphor::logging;
+
+SetParameterFromGroupMax::SetParameterFromGroupMax(
+    const json& jsonObj, const std::vector<Group>& groups) :
+    ActionBase(jsonObj, groups)
+{
+    setParameterName(jsonObj);
+    setModifier(jsonObj);
+}
+
+void SetParameterFromGroupMax::run(Zone& zone)
+{
+    std::optional<PropertyVariantType> max;
+
+    // Find the maximum value of all group member properties, possibly modify
+    // it, and then write it to the Manager as a parameter.
+
+    for (const auto& group : _groups)
+    {
+        const auto& members = group.getMembers();
+        for (const auto& member : members)
+        {
+            PropertyVariantType value;
+            try
+            {
+                value = Manager::getObjValueVariant(
+                    member, group.getInterface(), group.getProperty());
+            }
+            catch (const std::out_of_range&)
+            {
+                continue;
+            }
+
+            // Only allow a group to have multiple members if it's
+            // numeric. Unlike with std::is_arithmetic, bools are not
+            // considered numeric here.
+            if (members.size() > 1)
+            {
+                bool invalid = false;
+                std::visit(
+                    [&group, &invalid, this](auto&& val) {
+                        using V = std::decay_t<decltype(val)>;
+                        if constexpr (!std::is_same_v<double, V> &&
+                                      !std::is_same_v<int32_t, V> &&
+                                      !std::is_same_v<int64_t, V>)
+                        {
+                            log<level::ERR>(fmt::format("{}: Group {} has more "
+                                                        "than one member but "
+                                                        "isn't numeric",
+                                                        ActionBase::getName(),
+                                                        group.getName())
+                                                .c_str());
+                            invalid = true;
+                        }
+                    },
+                    value);
+                if (invalid)
+                {
+                    continue;
+                }
+            }
+
+            if (max && (value > max))
+            {
+                max = value;
+            }
+            else if (!max)
+            {
+                max = value;
+            }
+        }
+    }
+
+    if (_modifier)
+    {
+        try
+        {
+            *max = _modifier->doOp(*max);
+        }
+        catch (const std::exception& e)
+        {
+            log<level::ERR>(
+                fmt::format("{}: Could not perform modifier operation: {}",
+                            ActionBase::getName(), e.what())
+                    .c_str());
+            return;
+        }
+    }
+
+    Manager::setParameter(_name, *max);
+}
+
+void SetParameterFromGroupMax::setParameterName(const json& jsonObj)
+{
+    if (!jsonObj.contains("parameter_name"))
+    {
+        throw ActionParseError{ActionBase::getName(),
+                               "Missing required parameter_name value"};
+    }
+
+    _name = jsonObj["parameter_name"].get<std::string>();
+}
+
+void SetParameterFromGroupMax::setModifier(const json& jsonObj)
+{
+    if (jsonObj.contains("modifier"))
+    {
+        try
+        {
+            _modifier = std::make_unique<Modifier>(jsonObj.at("modifier"));
+        }
+        catch (const std::invalid_argument& e)
+        {
+            throw ActionParseError{ActionBase::getName(), e.what()};
+        }
+    }
+}
+
+}; // namespace phosphor::fan::control::json
