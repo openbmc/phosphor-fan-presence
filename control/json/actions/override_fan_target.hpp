c7836d62b5c30fde42ad646e56837792669809fa
Mike Capps <mikepcapps@gmail.com>
2021-09-09 10:36:08 -0400

    control - Action to override a single fan's speed within a zone
    When a single rotor fails in a multi-rotor fan, we want to be able to
    override the speed setting for the working rotor to maintain the best
    airflow environment possible. This change creates a new Action that
    triggers off the D-Bus propertiesChanged event, detects a non-functional
    state, and overrides all other speed settings to the max target
    available for that fan.
    
    Change-Id: I99f85645fae4e18254d289b9f026cea3a05320f5
    Signed-off-by: Mike Capps <mikepcapps@gmail.com>

---
+++ control/json/actions/override_fan_target.hpp
@@ -0,0 +1,120 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "../zone.hpp"
+#include "action.hpp"
+#include "group.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+
+/**
+ * @class OverrideFanTarget - Action to override spindle speed when another
+ * rotor fails
+ *
+ * When one spindle of a multi-rotor fan FRU fails, it is desirable to set the
+ * other spindle to max RPM, overriding any previous setting. This action is
+ * called when a failure is detected and forces the other spindle to its
+ * override setting.
+ */
+class OverrideFanTarget:
+    public ActionBase,
+    public ActionRegister<OverrideFanTarget>
+{
+  public:
+    /* Name of this action */
+    static constexpr auto name = "failure_override_target";
+
+    OverrideFanTarget() = delete;
+    OverrideFanTarget(const OverrideFanTarget&) = delete;
+    OverrideFanTarget(OverrideFanTarget&&) = delete;
+    OverrideFanTarget& operator=(const OverrideFanTarget&) = delete;
+    OverrideFanTarget& operator=(OverrideFanTarget&&) = delete;
+    ~OverrideFanTarget() = default;
+
+    /**
+     * @brief Set target when a number of members are at a given state
+     *
+     * @param[in] jsonObj - JSON configuration of this action
+     * @param[in] groups - Groups of dbus objects the action uses
+     */
+    OverrideFanTarget(const json& jsonObj,
+                          const std::vector<Group>& groups);
+
+    /**
+     * @brief Run the action
+     *
+     * Counts the number of members within a group are equal to a given state
+     * and when the number of members are at or above the given state, the fans
+     * within the zone are set to the configured target. The fans are held at
+     * the configured target until the number of members equal to the given
+     * state fall below the provided count.
+     *
+     * @param[in] zone - Zone to run the action on
+     */
+    void run(Zone& zone) override;
+
+  protected:
+    /* Instance id for each instance of this action */
+    static size_t instanceId;
+
+  private:
+    /* Number of group members */
+    size_t _count;
+
+    /* State the members must be at to set the target */
+    PropertyVariantType _state;
+
+    /* Target for this action */
+    uint64_t _target;
+
+    /* Unique id of this action */
+    size_t _id;
+
+    /**
+     * @brief Parse and set the count
+     *
+     * @param[in] jsonObj - JSON object for the action
+     *
+     * Sets the number of members that must equal the state
+     */
+    void setCount(const json& jsonObj);
+
+    /**
+     * @brief Parse and set the state
+     *
+     * @param[in] jsonObj - JSON object for the action
+     *
+     * Sets the state for each member to equal to set the target
+     */
+    void setState(const json& jsonObj);
+
+    /**
+     * @brief Parse and set the target
+     *
+     * @param[in] jsonObj - JSON object for the action
+     *
+     * Sets the target to use when running the action
+     */
+    void setTarget(const json& jsonObj);
+};
+
+} // namespace phosphor::fan::control::json
