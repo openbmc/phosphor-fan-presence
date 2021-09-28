b061845e36d814faab3ad15231ed51d0fd1c3902
Mike Capps <mikepcapps@gmail.com>
2021-09-27 22:32:46 -0400

    fanctl: add github link to help command
    Since man pages may not be available on the host, a link to the full
    fanctl readme is supplied in the help command.
    
    Signed-off-by: Mike Capps <mikepcapps@gmail.com>
    Change-Id: I146ac8c952cd846de5e910418ed2590982b01071

--- control/fanctl.cpp
+++ control/fanctl.cpp
@@ -512,6 +512,7 @@ void initCLI(CLI::App& app, uint64_t& target, std::vector<std::string>& fanList)
     // status method
     std::string strHelp("Prints fan target/tach readings, present/functional "
                         "states, and fan-monitor/BMC/Power service status");
+
     auto cmdStatus = commands->add_subcommand("status", strHelp);
     cmdStatus->set_help_flag("-h, --help", strHelp);
     cmdStatus->require_option(0);
@@ -559,8 +560,11 @@ int main(int argc, char* argv[])
 
     try
     {
-        CLI::App app{R"(Manually control, get fan tachs, view status, and resume
-             automatic control of all fans within a chassis.)"};
+        CLI::App app{"Manually control, get fan tachs, view status, and resume"
+                     " automatic control of all fans within a chassis.\n\
+\n\
+Full doccumentation can be found at the readme:\
+github.com/openbmc/phosphor-fan-presence/tree/master/control"};
 
         initCLI(app, target, fanList);
 
