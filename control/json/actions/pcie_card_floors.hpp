fca07fcde46f6ad157350099448932c4da79af4a
Matt Spinler <spinler@us.ibm.com>
2021-11-09 16:03:50 -0600

    control: PCIe floors action
    This action sets the PCIe floor index by looking up the highest floor
    index of all the powered on PCIe cards that are recognized in the PCIe
    card JSON files.  If a PCIe card has its own temperature sensor then it
    doesn't provide a floor index.  If a card isn't recognized, then the
    default floor index provided in the PCIe card JSON file is used.
    
    The class documentation contains additional details on how the action
    behaves.  It's JSON name is 'pcie_card_floors'.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I474916773476a6232d479119acb4ac2989909cb3

---
+++ control/json/actions/pcie_card_floors.hpp
@@ -0,0 +1,168 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "../zone.hpp"
+#include "action.hpp"
+#include "group.hpp"
+#include "utils/pcie_card_metadata.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace phosphor::fan::control::json
+{
+
+using json = nlohmann::json;
+
+class Manager;
+
+/**
+ * @class PCIeCardFloors - Action to set the PCIe card floor index parameter
+ *                          based on the PCIe cards plugged in the system.
+ *
+ *  - Loads PCIe card metadata files using the PCIeCardMetadata class.
+ *  - Watches for PCIe slots to be powered on (or off).
+ *  - Reads four properties off of the PCIeDevice interface on the powered
+ *    on cards.
+ *  - Looks up the floor index for the card by calling PCIeCardMetadata::lookup
+ *    and passing in the PCIeDevice properties.
+ *  - Sets the pcie_floor_index parameter with the highest floor index found.
+ *  - If no PCIe cards are found, it removes the parameter.
+ *  - If a card isn't recognized, the value configured as the default
+ *    index is used.
+ *  - If a powered on card has its own temperature sensor, then it doesn't
+ *    have a floor index.
+ *  - Since the slot powered on indications are all sent at once, it has a
+ *    small delay that gets started in each run() call that must expire
+ *    before the body of the action is run, so it only runs once.
+ *
+ *    The JSON configuration has two entries:
+ *    {
+ *       "base_pcie_card_file": <filename>
+ *       "use_config_specific_files": <true/false>
+ *    }
+ *
+ *    base_pcie_card_file:
+ *      - Used to specify a filename that doesn't depend on the system type.
+ *      - It can be omitted if use_config_specific_files is true.
+ *
+ *    use_config_specific_files:
+ *      - If true, will look for 'pcie_cards.json' files in the system
+ *        specific directories.
+ *      - See PCIeCardMetadata for details.
+ */
+class PCIeCardFloors : public ActionBase, public ActionRegister<PCIeCardFloors>
+{
+  public:
+    /* Name of this action */
+    static constexpr auto name = "pcie_card_floors";
+
+    PCIeCardFloors() = delete;
+    PCIeCardFloors(const PCIeCardFloors&) = delete;
+    PCIeCardFloors(PCIeCardFloors&&) = delete;
+    PCIeCardFloors& operator=(const PCIeCardFloors&) = delete;
+    PCIeCardFloors& operator=(PCIeCardFloors&&) = delete;
+    ~PCIeCardFloors() = default;
+
+    /**
+     * @brief Read in the action configuration
+     *
+     * @param[in] jsonObj - JSON configuration of this action
+     * @param[in] groups - Groups of dbus objects the action uses
+     */
+    PCIeCardFloors(const json& jsonObj, const std::vector<Group>& groups);
+
+    /**
+     * @brief Run the action.
+     *
+     * Starts/restarts the settle timer and then calls execute()
+     * when it expires.  Done to handle the flood of slot powered
+     * on/off signals.
+     *
+     * @param[in] zone - Zone to run the action on
+     */
+    void run(Zone& zone) override;
+
+  private:
+    /**
+     * @brief Runs the contents of the action when the settle timer expires.
+     *
+     * @param[in] zone - Zone to run the action on
+     */
+    void execute(Zone& zone);
+
+    /**
+     * @brief Constructs the PCIeCardMetadata object to load the PCIe card
+     *        JSON files.
+     *
+     * @param[in] jsonObj - JSON configuration of this action
+     */
+    void loadCardJSON(const json& jsonObj);
+
+    /**
+     * @brief Returns the D-Bus object path of the card plugged into
+     *        the slot represented by the slotPath D-Bus path.
+     *
+     * @param[in] slotPath - The D-Bus path of the PCIe slot object.
+     *
+     * @return const std::string& - The card object path.
+     */
+    const std::string& getCardFromSlot(const std::string& slotPath);
+
+    /**
+     * @brief Returns the floor index (or temp sensor name) for the
+     *        card in the passed in slot.
+     *
+     * @param[in] slotPath - The D-Bus path of the PCIe slot object.
+     *
+     * @return optional<variant<int32_t, string>>
+     *  - The floor index or true for has temp sensor if found,
+     *    std::nullopt else.
+     */
+    std::optional<std::variant<int32_t, bool>>
+        getFloorIndexFromSlot(const std::string& slotPath);
+
+    /**
+     * @brief Gets the hex PCIeDevice property value from the
+     *        manager object cache.
+     *
+     * @param[in] objectPath - The card object path
+     * @param[in] propertyName - The property to read
+     *
+     * @return uint16_t The property value
+     */
+    uint16_t getPCIeDeviceProperty(const std::string& objectPath,
+                                   const std::string& propertyName);
+
+    /* The PCIe card metadata manager */
+    std::unique_ptr<PCIeCardMetadata> _cardMetadata;
+
+    /* Cache map of PCIe slot paths to their plugged card paths */
+    std::unordered_map<std::string, std::string> _cards;
+
+    /* Cache of all objects with a PCIeDevice interface. */
+    std::vector<std::string> _pcieDevices;
+
+    std::chrono::seconds _settleTime{0};
+
+    /* Timer to wait for slot plugs to settle down before running action */
+    std::unique_ptr<Timer> _settleTimer;
+
+    /* Last status printed so only new messages get recorded */
+    std::string _lastStatus;
+};
+
+} // namespace phosphor::fan::control::json
