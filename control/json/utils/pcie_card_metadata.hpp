b6036b4394dfb27c4e214ad7b99c0f2fab407e8e
Matt Spinler <spinler@us.ibm.com>
2021-11-09 13:27:26 -0600

    control: PCIe card metadata wrapper class
    This class introduces a PCIeCardMetadata class to manage JSON files
    containing PCIe card floor indexes. These can then be used by actions
    that want to set the fan floors based on which PCIe cards are present.
    
    It provides a lookup() method that takes four properties from the
    PCIeDevice D-Bus interface to uniquely identify a card, and then returns
    the floor index for the card if it doesn't have a temperature sensor on
    it, or the expected temperature sensor D-Bus name.
    
    Its constructor takes a base config file name, and the vector of system
    names usually filled in by entity-manager using the IBMCompatibleSystem
    interface.  If the base config file name was filled in, it will load
    that one first from /usr/share/phosphor-fan-presence/control, and then
    traverse the system names list in reverse, looking in
    /usr/share/phosphor-fan-presence/control/<system name>/pcie_cards.json.
    
    It will overwrite any file entries that match as it goes on, so if a
    system has to override just a few of the floor indexes from a more
    generic file it can be done without having to duplicate entries for
    cards that are the same.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I81f2476dd58d1529ee6484243e7d8f1e49027cf9

---
+++ control/json/utils/pcie_card_metadata.hpp
@@ -0,0 +1,173 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include <filesystem>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace phosphor::fan::control::json
+{
+
+/**
+ * @class PCIeCardMetadata
+ *
+ * This class provides the ability for an action to look up a PCIe card's
+ * fan floor index or temp sensor name based on its metadata, which
+ * consists of 4 properties from the PCIeDevice D-Bus interface.
+ *
+ * The metadata is stored in one or more (see loadCards) JSON files, which
+ * look like:
+ *  {
+ *    "default_floor_index": 8,
+ *    "cards": [
+ *      {
+ *        "comment": "TestCard",
+ *        "device_id": "0x1",
+ *        "vendor_id": "0x2",
+ *        "subsystem_id": "0x3",
+ *        "subsystem_vendor_id": "0x4",
+ *        "floor_index": 3
+ *      },
+ *      ...
+ *    ]
+ *  }
+ *
+ * If the card has a temperature sensor on it, then it doesn't
+ * need a floor index and instead will have:
+ *   "has_temp_sensor": true,
+ *   "sensor_name": "Card_A_\d+_Temp"
+ *
+ * The sensor_name field above is a regulator expression that can be
+ * used to match on its D-Bus temperature sensor names if necessary.
+ *
+ * The default_floor_index field can be used by the action when
+ * the lookup doesn't find a match.
+ */
+class PCIeCardMetadata
+{
+  public:
+    PCIeCardMetadata() = delete;
+    ~PCIeCardMetadata() = default;
+    PCIeCardMetadata(const PCIeCardMetadata&) = delete;
+    PCIeCardMetadata& operator=(const PCIeCardMetadata&) = delete;
+    PCIeCardMetadata(PCIeCardMetadata&&) = delete;
+    PCIeCardMetadata& operator=(PCIeCardMetadata&&) = delete;
+
+    /**
+     * @brief Constructor
+     *
+     * @param[in] baseConfigFile - The first config file to load
+     * @param[in] systemNames - The IBMCompatible names values
+     */
+    PCIeCardMetadata(const std::string& baseConfigFile,
+                     const std::vector<std::string>& systemNames);
+
+    /**
+     * @brief Look up a floor index based on a card's metadata
+     *
+     * @param[in] deviceID - Function0DeviceId value
+     * @param[in] vendorID - Function0VendorId value
+     * @param[in] subsystemID - Function0SubsystemId value
+     * @param[in] subsystemVendorID - Function0SubsystemVendorId value
+     *
+     * @return optional<variant<int32, string>> -
+     *     Either the floor index for that entry, or the name of the
+     *     D-Bus temp sensor it will expose if found.
+     *
+     *     If no entry is found, it will return std::nullopt.
+     */
+    std::optional<std::variant<int32_t, std::string>>
+        lookup(uint16_t deviceID, uint16_t vendorID, uint16_t subsystemID,
+               uint16_t subsystemVendorID) const;
+
+    /**
+     * @brief Returns the default floor index in the JSON
+     */
+    int32_t getDefaultFloorIndex() const
+    {
+        return _defaultFloorIndex;
+    }
+
+  private:
+    /**
+     * Structure to hold card metadata.
+     */
+    struct Metadata
+    {
+        uint16_t vendorID;
+        uint16_t deviceID;
+        uint16_t subsystemVendorID;
+        uint16_t subsystemID;
+        int32_t floorIndex;
+        bool hasTempSensor;
+        std::string sensorNameRegex;
+
+        bool operator==(const Metadata& other)
+        {
+            return (vendorID == other.vendorID) &&
+                   (deviceID == other.deviceID) &&
+                   (subsystemVendorID == other.subsystemVendorID) &&
+                   (subsystemID == other.subsystemID);
+        }
+    };
+
+    /**
+     * @brief Loads the metadata from JSON files
+     *
+     * If baseConfigFile isn't empty, it will load in that file
+     *
+     * If systemNames isn't empty, it will load in any 'pcie_cards.json'
+     * files it finds in directories based on those names, overwriting any
+     * entries that were in any previous files.  This allows different
+     * floor indexes for some cards for the different systems in a flash
+     * image without needing to specify every possible card in every
+     * system specific JSON file.
+     *
+     * @param[in] baseConfigFile - The first config file to load
+     * @param[in] systemNames - The IBMCompatible names values
+     */
+    void loadCards(const std::filesystem::path& baseConfigFile,
+                   const std::vector<std::string>& systemNames);
+
+    /**
+     * @brief Loads in the card info from the JSON
+     *
+     * @param[in] json - The JSON containing a cards array
+     */
+    void load(const nlohmann::json& json);
+
+    /**
+     * @brief Dumps the cards vector for debug
+     */
+    void dump() const;
+
+    /**
+     * @brief The card metadata
+     */
+    std::vector<Metadata> _cards;
+
+    /**
+     * @brief The default floor index for when the lookup fails
+     */
+    int32_t _defaultFloorIndex{0};
+};
+
+} // namespace phosphor::fan::control::json
