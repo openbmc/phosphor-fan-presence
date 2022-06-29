e1b7cbaed5c2e44759523da3b185cda9a2d97ca4
Mike Capps <mikepcapps@gmail.com>
2022-06-29 14:23:07 -0400

    meson support: configuration option removal
    D-Bus names and paths that were previously configurable at build-time
    are now hard-coded and moved to dbus_paths.hpp to reduce the number of
    configure-time options and simplify maintenance.
    
    Signed-off-by: Mike Capps <mikepcapps@gmail.com>
    Change-Id: I16d88daad90e747cc40d87c853874b1a5fedf5fa

---
+++ dbus_paths.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+
+// Global constants from Phosphor Fan Presence's D-Bus names and paths
+
+// Control Application's D-Bus busname to own
+static constexpr char CONTROL_BUSNAME[] = "xyz.openbmc_project.Control.Thermal";
+
+// Control Application's root D-Bus object path
+static constexpr char CONTROL_OBJPATH[] =
+    "/xyz/openbmc_project/control/thermal";
+
+// Sensor-Monitor Application's root D-Bus object path
+static constexpr char SENSOR_MONITOR_PERSIST_ROOT_PATH[] =
+    "/xyz/openbmc_project/alerts/thermal_fault_alert";
+
+// Thermal Application's D-Bus busname to own
+static constexpr char THERMAL_ALERT_BUSNAME[] =
+    "xyz.openbmc_project.Thermal.Alert";
+
+// Thermal Application's root D-Bus object path
+static constexpr char THERMAL_ALERT_OBJPATH[] =
+    "/xyz/openbmc_project/alerts/thermal_fault_alert";
