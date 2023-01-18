edd74a29bd7cbe61fcf680b765d70cf6de4f5842
Delphine CC Chiu <Bonnie_Lo@wiwynn.com>
2023-01-18 16:33:04 +0800

    sensor-mon: Support recovery action
    The sensor-monitor could only watch any SoftShutdown or HardShutdown
    alarm to trigger the target for shutdown hosts (Protection). To support
    recovery action for different sensors, we refactored the
    ShutdownAlarmMonitor, split the 3 major responsibilities into classes:
    1. Watch specific dbus alarm properties. -- DbusAlarmMonitor
    2. Filter the alarms we don't need. -- AlarmChecker
    3. Handle alarms. -- AlarmHandler
    
    Recovery detection only activate when protection is triggered, we use
    DomainEventPublisher and RecoverySubscriber (observer pattern) to send
    & catch both the protection and recovery triggered events, notify
    DbusAlarmMonitor the watch/stop watching specific sensor's alarms.
    
    To avoid the unstable state of recovery's alarm, RecoveryAlarmHandler
    will start a timer when the alarm is de-assert (the time can be
    configured, 0 as default.), if the alarm is assert again, the timer will
    stop, the recovery action won't be triggered until the the alarm is
    stable.
    
    Different projects can define what sensor & alarm should be
    monitored by configuring the recovery-action.json. The recovery
    detection can also be enabled/disabled by setting the meson config.
    
    Signed-off-by: Bonnie Lo <Bonnie_Lo@wiwynn.com>
    Change-Id: If592323a78282eeba5ccede6626d0e0008a299d9

---
+++ sensor-monitor/domain_event_subscribers/recovery_subscriber.hpp
@@ -0,0 +1,136 @@
+#pragma once
+#include "config.h"
+#include "domain_event_subscriber.hpp"
+#include "dbus_alarm_monitor.hpp"
+#include "types.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+#include <nlohmann/json.hpp>
+#include <typeinfo>
+#include <vector>
+#include <string>
+
+namespace sensor::monitor
+{
+using namespace phosphor::logging;
+using json = nlohmann::json;
+
+class RecoverySubscriber: public DomainEventSubscriber
+{
+	public:
+    RecoverySubscriber(DbusAlarmMonitor& dbusAlarmMonitor):
+      _dbusAlarmMonitor(dbusAlarmMonitor)
+    {
+      loadRecoveryActionConfig();
+    }
+
+    void handle(DomainEvent& event) override final
+    {
+      if (typeid(event) == typeid(SensorProtectionTriggered))
+      {
+        auto& theEvent = dynamic_cast<SensorProtectionTriggered&>(event);
+        std::string sensorPath = theEvent.getSensorPath();
+        for(auto& recoveryType: obtainRecoveryTypesOf(sensorPath))
+        {
+          _dbusAlarmMonitor.watchSensorAlarm(sensorPath, recoveryType);
+        }
+      }
+      else if (typeid(event) == typeid(SensorRecoveryTriggered))
+      {
+        auto& theEvent = dynamic_cast<SensorRecoveryTriggered&>(event);
+        _dbusAlarmMonitor.stopWatchAlarm(theEvent.getSensorPath(),
+          theEvent.getAlarmType());
+      }
+    }
+
+  private:
+    std::vector<AlarmType> obtainRecoveryTypesOf(std::string sensorPath)
+    {
+      std::vector<AlarmType> result;
+
+      auto it = _sensorsRecoveryThresholds.find(sensorPath);
+      if (it == _sensorsRecoveryThresholds.end())
+      {
+        return result;
+      }
+
+      for(auto& [threshold, delay]: it->second)
+      {
+        if (threshold == "CriticalAlarmLow" || threshold == "CriticalAlarmHigh")
+        {
+          result.emplace_back(AlarmType::critical);
+        }
+        else if (threshold == "WarningAlarmLow" || threshold == "WarningAlarmHigh")
+        {
+          result.emplace_back(AlarmType::warning);
+        }
+        else
+        {
+          log<level::ERR>(fmt::format("{} is not match to any\
+              threshold interface", threshold).c_str());
+        }
+      }
+
+      return result;
+    }
+
+    void parse(json& recoveryConfig)
+    {
+      for (auto& sensorConfig: recoveryConfig.at("sensors"))
+      {
+        try
+        {
+          std::string sensorPath = sensorConfig.at("path");
+          std::map<std::string, int> thresholds;
+          for (auto& threshold: sensorConfig.at("thresholds"))
+          {
+            thresholds.emplace(threshold.at("alarm"),
+              threshold.at("stableCountdown"));
+          }
+
+          _sensorsRecoveryThresholds.emplace(sensorPath, thresholds);
+        }
+        catch (const std::exception& e)
+        {
+          log<level::ERR>(fmt::format("Failed to parse config file: {}", e.what()).c_str());
+        }
+      }
+    }
+
+    void loadRecoveryActionConfig()
+    {
+      std::filesystem::path path =
+            std::filesystem::path{SENSOR_MONITOR_PERSIST_ROOT_PATH} /
+            recoveryConfigName;
+
+      if (!std::filesystem::exists(path))
+      {
+        log<level::ERR>(fmt::format("Config file: {}/{} don't exist",
+          SENSOR_MONITOR_PERSIST_ROOT_PATH, recoveryConfigName).c_str());
+        return;
+      }
+
+      std::ifstream configFile(path.c_str());
+      json recoveryConfig = json::parse(configFile);
+      if (recoveryConfig.empty())
+      {
+        configFile.close();
+        log<level::INFO>("The configuration is empty.");
+        return;
+      }
+      configFile.close();
+
+      parse(recoveryConfig);
+
+      return;
+    }
+
+    DbusAlarmMonitor& _dbusAlarmMonitor;
+
+    std::map<std::string, std::map<std::string, int>> _sensorsRecoveryThresholds;
+
+    const std::string recoveryConfigName = "recovery-action.json";
+};
+
+} // namespace sensor::monitor
