b5ce33fd024428e00fc44eb50d8de31cb363cfe7
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2023-01-18 16:34:32 +0800

    sensor-mon: Support recovery action
    The sensor-monitor could only watch any SoftShutdown or HardShutdown
    alarm to trigger the target for shutdown hosts (Protection). To support
    recovery action for different sensors, we refactored the
    ShutdownAlarmMonitor, split the 3 major responsibilities into classes:
    1. Watch specific dbus alarm properties. -- DbusAlarmMonitor
    2. Filter the alarms we don't need. -- AlarmChecker
    3. Handle alarms. -- AlarmHandler
    
    Recovery detection only activate when protection is triggered, we use
    DomainEventPublisher and RecoverySubscriber (observer pattern) to send
    & catch both the protection and recovery triggered events, notify
    DbusAlarmMonitor the watch/stop watching specific sensor's alarms.
    
    To avoid the unstable state of recovery's alarm, RecoveryAlarmHandler
    will start a timer when the alarm is de-assert (the time can be
    configured, 0 as default.), if the alarm is assert again, the timer will
    stop, the recovery action won't be triggered until the the alarm is
    stable.
    
    Different projects can define what sensor & alarm should be
    monitored by configuring the recovery-action.json. The recovery
    detection can also be enabled/disabled by setting the meson config.
    
    Signed-off-by: Bonnie Lo <Bonnie_Lo@wiwynn.com>
    Change-Id: If592323a78282eeba5ccede6626d0e0008a299d9

---
+++ sensor-monitor/domain_event_subscribers/domain_event_subscriber.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "domain_events.hpp"
+
+namespace sensor::monitor
+{
+
+class DomainEventSubscriber
+{
+	public:
+		virtual ~DomainEventSubscriber() = default;
+		virtual void handle(DomainEvent& event) = 0;
+};
+
+} // namespace sensor::monitor
