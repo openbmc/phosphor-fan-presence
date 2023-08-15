acc66d02e33c55479b1af0de9f1b8c9620173c2a
George Liu <liuxiwei@inspur.com>
2023-08-15 17:10:01 +0800

    cooling-type: Replace Argument class with CLI11
    Phosphor-fan already depends on cli11, delete Argument class and use
    cli11 instead.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I327a522ecf232c00661088f2c527e626c24610bc

--- cooling-type/main.cpp
+++ cooling-type/main.cpp
@@ -1,9 +1,9 @@
-#include "argument.hpp"
 #include "cooling_type.hpp"
 #include "sdbusplus.hpp"
 
 #include <fmt/format.h>
 
+#include <CLI/CLI.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 
@@ -17,72 +17,84 @@ using namespace phosphor::logging;
 int main(int argc, char* argv[])
 {
     auto rc = 1;
-    auto options = ArgumentParser(argc, argv);
 
-    auto objpath = (options)["path"];
-    if (argc < 2)
+    CLI::App app{"Phosphor Cooling type"};
+    std::string objpath{};
+    bool air = false;
+    bool water = false;
+    std::string gpiopath{};
+    std::string keycode{};
+
+    app.add_option("-p,--path", objpath,
+                   "object path under inventory to have CoolingType updated")
+        ->required();
+    app.add_flag("-a,--air", air,
+                 "Force 'AirCooled' property to be set to true.");
+    app.add_flag("-w,--water", water,
+                 "Force 'WaterCooled' property to be set to true.");
+    app.add_option(
+        "-d,--dev", gpiopath,
+        "Device to read for GPIO pin state to determine 'WaterCooled' (true) and 'AirCooled' (false)");
+    app.add_option("-e,--event", keycode, "Keycode for pin to read");
+    app.allow_extras();
+    try
     {
-        std::cerr << std::endl << "Too few arguments" << std::endl;
-        log<level::ERR>("Too few arguments");
-        options.usage(argv);
+        app.parse(argc, argv);
     }
-    else if (objpath == ArgumentParser::empty_string)
+    catch (const CLI::Error& e)
+    {
+        return app.exit(e);
+    }
+
+    if (objpath.empty())
     {
         log<level::ERR>("Bus path argument required");
+        return rc;
     }
-    else
+
+    auto bus = sdbusplus::bus::new_default();
+    CoolingType coolingType(bus);
+
+    try
     {
-        auto bus = sdbusplus::bus::new_default();
-        CoolingType coolingType(bus);
-
-        try
+        if (air)
         {
-            auto air = (options)["air"];
-            if (air != ArgumentParser::empty_string)
-            {
-                coolingType.setAirCooled();
-            }
-
-            auto water = (options)["water"];
-            if (water != ArgumentParser::empty_string)
-            {
-                coolingType.setWaterCooled();
-            }
-
-            auto gpiopath = (options)["dev"];
-            if (gpiopath != ArgumentParser::empty_string)
-            {
-                auto keycode = (options)["event"];
-                if (keycode != ArgumentParser::empty_string)
-                {
-                    auto gpiocode = std::stoul(keycode);
-                    coolingType.readGpio(gpiopath, gpiocode);
-                }
-                else
-                {
-                    log<level::ERR>("--event=<keycode> argument required\n");
-                    return rc;
-                }
-            }
-
-            coolingType.updateInventory(objpath);
-            rc = 0;
+            coolingType.setAirCooled();
         }
-        catch (const DBusMethodError& dme)
+
+        if (water)
         {
-            log<level::ERR>(
-                fmt::format("Uncaught DBus method failure exception "
-                            "Busname: {} "
-                            "Path: {} "
-                            "Interface: {} "
-                            "Method: {}",
-                            dme.busName, dme.path, dme.interface, dme.method)
-                    .c_str());
+            coolingType.setWaterCooled();
         }
-        catch (const std::exception& err)
+
+        if (!gpiopath.empty())
         {
-            log<phosphor::logging::level::ERR>(err.what());
+            if (keycode.empty())
+            {
+                log<level::ERR>("--event=<keycode> argument required\n");
+                return rc;
+            }
+            auto gpiocode = std::stoul(keycode);
+            coolingType.readGpio(gpiopath, gpiocode);
         }
+
+        coolingType.updateInventory(objpath);
+        rc = 0;
+    }
+    catch (const DBusMethodError& dme)
+    {
+        log<level::ERR>(fmt::format("Uncaught DBus method failure exception "
+                                    "Busname: {} "
+                                    "Path: {} "
+                                    "Interface: {} "
+                                    "Method: {}",
+                                    dme.busName, dme.path, dme.interface,
+                                    dme.method)
+                            .c_str());
+    }
+    catch (const std::exception& err)
+    {
+        log<phosphor::logging::level::ERR>(err.what());
     }
 
     return rc;
