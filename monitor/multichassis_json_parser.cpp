1da5195f7b5aed7bfac4bec1208d4fa73a26ba92
Anwaar Hadi <anwaar.hadi@ibm.com>
2025-09-08 21:46:45 +0000

    JSON Parsing Code for Multi-Chassis Monitor App
    Write functions for parsing JSON config file for the multi-chassis
    version of the fan monitor application. Also write unit tests for
    said functions.
    
    Summary of JSON Design Changes
    
    Overall Layout:
    
    ```
    {
        "fan_type_definitions": [
            {
                ...,
                "sensors": [
                    ...
                ]
            }
        ],
        "chassis_definitions": [
            {
                "chassis_numbers": [
                    ...
                ],
                "zones": [
                    {
                        ...,
                        "fans": [
                            ...
                        ],
                        "fault_handling": {
                            ...
                        }
                    }
                ]
            }
        ]
    }
    ```
    Descriptions of Different Fields:
    * "fan_type_definitions" defines configurations for specific types of
    fans. Most fields are carried over from the previous JSON design, with
    the inventory path field being instead replaced by a fan type name
    ("type").
    
    * "sensors" defines a list of sensors assigned to a specific fan type.
    Each sensor has a "path_suffix" tag that should be appended to the
    "inventory_base" and "short_name" properties to form the full path for
    the sensor. Other fields are carried over from the previous design.
    
    * "chassis_definitions" specifies configurations that apply to a group
    of chassis ("chassis_numbers"). Chassis are divided into "zones" which
    each have their own fan assignments ("fans"). Power off rules
    ("fault_handling") are also defined at the zone-level (as opposed to the
    system level in previous design).
    
    Tested:
    * Ensure local CI and developed unit tests passed.
    
    Change-Id: I6e35c1655625b190718af4c885d3aebc6a8cc9b5
    Signed-off-by: Anwaar Hadi <anwaar.hadi@ibm.com>

---
+++ monitor/multichassis_json_parser.cpp
@@ -0,0 +1,427 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "multichassis_json_parser.hpp"
+
+#include "conditions.hpp"
+#include "dbus_paths.hpp"
+#include "nonzero_speed_trust.hpp"
+#include "power_interface.hpp"
+#include "power_off_rule.hpp"
+#include "system.hpp"
+#include "tach_sensor.hpp"
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <optional>
+#include <vector>
+
+namespace phosphor::fan::monitor::multi_chassis
+{
+const std::map<std::string, condHandler> conditions = {
+    {"propertiesmatch", monitor::condition::getPropertiesMatch}};
+const std::map<std::string, size_t> methods = {
+    {"timebased", MethodMode::timebased}, {"count", MethodMode::count}};
+
+const std::vector<SensorDefinition> getSensorDefs(const json& sensors)
+{
+    std::vector<SensorDefinition> sensorDefs;
+
+    for (const auto& sensor : sensors)
+    {
+        if (!sensor.contains("path_suffix") || !sensor.contains("has_target"))
+        {
+            // Log error on missing required parameters
+            lg2::error(
+                "Missing required fan sensor definition parameters 'path_suffix, has_target'");
+            throw std::runtime_error(
+                "Missing required fan sensor definition parameters");
+        }
+        // Target interface is optional and defaults to
+        // 'xyz.openbmc_project.Control.FanSpeed'
+        std::string targetIntf = "xyz.openbmc_project.Control.FanSpeed";
+        if (sensor.contains("target_interface"))
+        {
+            targetIntf = sensor["target_interface"].get<std::string>();
+        }
+        // Target path is optional
+        std::string targetPath;
+        if (sensor.contains("target_path"))
+        {
+            targetPath = sensor["target_path"].get<std::string>();
+        }
+        // Factor is optional and defaults to 1
+        double factor = 1.0;
+        if (sensor.contains("factor"))
+        {
+            factor = sensor["factor"].get<double>();
+        }
+        // Offset is optional and defaults to 0
+        int64_t offset = 0;
+        if (sensor.contains("offset"))
+        {
+            offset = sensor["offset"].get<int64_t>();
+        }
+        // Threshold is optional and defaults to 1
+        size_t threshold = 1;
+        if (sensor.contains("threshold"))
+        {
+            threshold = sensor["threshold"].get<size_t>();
+        }
+        // Ignore being above the allowed max is optional, defaults to not
+        bool ignoreAboveMax = false;
+        if (sensor.contains("ignore_above_max"))
+        {
+            ignoreAboveMax = sensor["ignore_above_max"].get<bool>();
+        }
+
+        SensorDefinition def{
+            .pathSuffix = sensor["path_suffix"].get<std::string>(),
+            .hasTarget = sensor["has_target"].get<bool>(),
+            .targetInterface = targetIntf,
+            .targetPath = targetPath,
+            .factor = factor,
+            .offset = offset,
+            .threshold = threshold,
+            .ignoreAboveMax = ignoreAboveMax};
+
+        sensorDefs.push_back(std::move(def));
+    }
+    return sensorDefs;
+}
+const std::vector<FanTypeDefinition> getFanDefs(const json& fans)
+{
+    std::vector<FanTypeDefinition> fanDefs;
+
+    for (const auto& fan : fans)
+    {
+        if (!fan.contains("type") || !fan.contains("deviation") ||
+            !fan.contains("sensors"))
+        {
+            lg2::error(
+                "Missing one of required parameters 'type, deviation, sensors'");
+            throw std::runtime_error(
+                "Missing required fan type definition parameter");
+        }
+
+        // Valid deviaiton range is 0 - 100%
+        auto deviation = fan["deviation"].get<size_t>();
+        if (deviation > 100)
+        {
+            lg2::error(
+                "Invalid deviation of {DEVIATION} found, must be between 0 and 100",
+                "DEVIATION", deviation);
+            throw std::runtime_error(
+                "Invalid deviation found, must be between 0 and 100");
+        }
+
+        // Upper deviation defaults to the deviation value, but can be
+        // separately specified
+        size_t upperDeviation = deviation;
+        if (fan.contains("upper_deviation"))
+        {
+            upperDeviation = fan["upper_deviation"].get<size_t>();
+            if (upperDeviation > 100)
+            {
+                lg2::error(
+                    "Invalid upper_deviation of {UPPER_DEVIATION} found, must be between 0 and 100",
+                    "UPPER_DEVIATION", upperDeviation);
+                throw std::runtime_error(
+                    "Invalid upper_deviation found, must be between 0 and 100");
+            }
+        }
+
+        // Construct the sensor definitions for this fan
+        auto sensorDefs = getSensorDefs(fan["sensors"]);
+
+        // Functional delay is optional and defaults to 0
+        size_t funcDelay = 0;
+        if (fan.contains("functional_delay"))
+        {
+            funcDelay = fan["functional_delay"].get<size_t>();
+        }
+
+        // Method is optional and defaults to time based functional
+        // determination
+        size_t method = MethodMode::timebased;
+        size_t countInterval = 1;
+        if (fan.contains("method"))
+        {
+            auto methodConf = fan["method"].get<std::string>();
+            auto methodFunc = methods.find(methodConf);
+            if (methodFunc != methods.end())
+            {
+                method = methodFunc->second;
+            }
+            else
+            {
+                // Log error on unsupported method parameter
+                lg2::error("Invalid fan method");
+                throw std::runtime_error("Invalid fan method");
+            }
+
+            // Read the count interval value used with the count method.
+            if (method == MethodMode::count)
+            {
+                if (fan.contains("count_interval"))
+                {
+                    countInterval = fan["count_interval"].get<size_t>();
+                }
+            }
+        }
+
+        // Timeout defaults to 0
+        size_t timeout = 0;
+        if (method == MethodMode::timebased)
+        {
+            if (!fan.contains("allowed_out_of_range_time"))
+            {
+                // Log error on missing required parameter
+                lg2::error(
+                    "Missing required fan monitor definition parameters 'allowed_out_of_range_time'");
+                throw std::runtime_error(
+                    "Missing required fan monitor definition parameters");
+            }
+            else
+            {
+                timeout = fan["allowed_out_of_range_time"].get<size_t>();
+            }
+        }
+
+        // Monitor start delay is optional and defaults to 0
+        size_t monitorDelay = 0;
+        if (fan.contains("monitor_start_delay"))
+        {
+            monitorDelay = fan["monitor_start_delay"].get<size_t>();
+        }
+
+        // num_sensors_nonfunc_for_fan_nonfunc is optional and defaults
+        // to zero if not present, meaning the code will not set the
+        // parent fan to nonfunctional based on sensors.
+        size_t nonfuncSensorsCount = 0;
+        if (fan.contains("num_sensors_nonfunc_for_fan_nonfunc"))
+        {
+            nonfuncSensorsCount =
+                fan["num_sensors_nonfunc_for_fan_nonfunc"].get<size_t>();
+        }
+
+        // nonfunc_rotor_error_delay is optional and defaults to 0
+        size_t nonfuncRotorErrorDelay = 0;
+        if (fan.contains("nonfunc_rotor_error_delay"))
+        {
+            nonfuncRotorErrorDelay =
+                fan["nonfunc_rotor_error_delay"].get<size_t>();
+        }
+
+        // fan_missing_error_delay is optional.
+        std::optional<size_t> fanMissingErrorDelay;
+        if (fan.contains("fan_missing_error_delay"))
+        {
+            fanMissingErrorDelay =
+                fan.at("fan_missing_error_delay").get<size_t>();
+        }
+
+        // Handle optional conditions
+        auto cond = std::optional<Condition>();
+        if (fan.contains("condition"))
+        {
+            if (!fan["condition"].contains("name"))
+            {
+                // Log error on missing required parameter
+                lg2::error(
+                    "Missing required fan monitor condition parameter 'name'");
+                throw std::runtime_error(
+                    "Missing required fan monitor condition parameter");
+            }
+            auto name = fan["condition"]["name"].get<std::string>();
+            // The function for fan monitoring condition
+            // (Must have a supported function within the condition namespace)
+            std::transform(name.begin(), name.end(), name.begin(), tolower);
+            auto handler = conditions.find(name);
+            if (handler != conditions.end())
+            {
+                cond = handler->second(fan["condition"]);
+            }
+            else
+            {
+                lg2::info(
+                    "No handler found for configured condition {CONDITION_NAME}",
+                    "CONDITION_NAME", name, "JSON_DUMP",
+                    fan["condition"].dump());
+            }
+        }
+
+        // if the fan should be set to functional when plugged in
+        bool setFuncOnPresent = false;
+        if (fan.contains("set_func_on_present"))
+        {
+            setFuncOnPresent = fan["set_func_on_present"].get<bool>();
+        }
+
+        FanTypeDefinition def{
+            .type = fan["type"].get<std::string>(),
+            .method = method,
+            .funcDelay = funcDelay,
+            .timeout = timeout,
+            .deviation = deviation,
+            .upperDeviation = upperDeviation,
+            .numSensorFailsForNonfunc = nonfuncSensorsCount,
+            .monitorStartDelay = monitorDelay,
+            .countInterval = countInterval,
+            .nonfuncRotorErrDelay = nonfuncRotorErrorDelay,
+            .fanMissingErrDelay = fanMissingErrorDelay,
+            .sensorList = std::move(sensorDefs),
+            .condition = cond,
+            .funcOnPresent = setFuncOnPresent};
+
+        fanDefs.push_back(std::move(def));
+    }
+
+    return fanDefs;
+}
+
+const std::vector<ChassisDefinition> getChassisDefs(const json& chassis)
+{
+    std::vector<ChassisDefinition> chassisDefs;
+    for (const auto& chassisConfig : chassis)
+    {
+        if (!chassisConfig.contains("chassis_numbers") ||
+            !chassisConfig.contains("zones"))
+        {
+            lg2::error(
+                "Missing one of required fields 'chassis_numbers, zones'");
+            throw std::runtime_error("Missing required field");
+        }
+
+        std::vector<int> chassisNums =
+            chassisConfig["chassis_numbers"].get<std::vector<int>>();
+        if (!std::ranges::all_of(chassisNums, [](int num) { return num >= 0; }))
+        {
+            // Throw error for negative chassis number
+            throw std::runtime_error(
+                "Chassis numbers must be greater than or equal to 0");
+        }
+        std::vector<ZoneDefinition> zoneDefs =
+            getZoneDefs(chassisConfig["zones"], chassisNums);
+        ChassisDefinition def{.chassisNumbers = std::move(chassisNums),
+                              .zones = std::move(zoneDefs)};
+
+        chassisDefs.push_back(std::move(def));
+    }
+
+    return chassisDefs;
+}
+
+const std::vector<ZoneDefinition> getZoneDefs(
+    const json& zones, const std::vector<int>& chassisNums)
+{
+    std::vector<ZoneDefinition> zoneDefs;
+    std::string chassisHolder = "${chassis}";
+    for (const auto& zoneConfig : zones)
+    {
+        if (!zoneConfig.contains("name") || !zoneConfig.contains("fans") ||
+            !zoneConfig.contains("fault_handling"))
+        {
+            lg2::error(
+                "Missing one of required fields 'name, fans, fault_handling'");
+            throw std::runtime_error("Missing required field");
+        }
+        std::string name = zoneConfig["name"].get<std::string>();
+        if (name.contains(chassisHolder))
+        {
+            // expand on the chassis_nums list
+            for (int n : chassisNums)
+            {
+                std::string fullName = name;
+                fullName.replace(name.find(chassisHolder),
+                                 chassisHolder.length(), std::to_string(n));
+                std::vector<FanAssignment> fanAssigns =
+                    getFanAssigns(zoneConfig["fans"], n);
+                ZoneDefinition def{
+                    .name = fullName,
+                    .fans = std::move(fanAssigns),
+                    .faultHandling = zoneConfig["fault_handling"]};
+                zoneDefs.push_back(def);
+            }
+        }
+        else
+        {
+            // don't expand, simply push zone object to zoneDefs
+            std::vector<FanAssignment> fanAssigns =
+                getFanAssigns(zoneConfig["fans"], std::nullopt);
+            ZoneDefinition def{.name = name,
+                               .fans = std::move(fanAssigns),
+                               .faultHandling = zoneConfig["fault_handling"]};
+            zoneDefs.push_back(def);
+        }
+    }
+    return zoneDefs;
+}
+
+const std::vector<FanAssignment> getFanAssigns(const json& fanList,
+                                               std::optional<int> chassisNum)
+{
+    std::vector<FanAssignment> fanAssigns;
+    std::string chassisHolder = "${chassis}";
+    if (chassisNum.has_value())
+    {
+        // replace chassis value
+        for (const auto& fan : fanList)
+        {
+            if (!fan.contains("type") || !fan.contains("inventory_base") ||
+                !fan.contains("short_name"))
+            {
+                lg2::error(
+                    "Missing one of required fields 'type, inventory_base, short_name' in fan object");
+                throw std::runtime_error(
+                    "Missing required field in fan object");
+            }
+            std::string inventoryBase =
+                fan["inventory_base"].get<std::string>();
+            std::string shortName = fan["short_name"].get<std::string>();
+            size_t chassisHolderPlace = inventoryBase.find(chassisHolder);
+            if (chassisHolderPlace != std::string::npos)
+            {
+                inventoryBase.replace(chassisHolderPlace,
+                                      chassisHolder.length(),
+                                      std::to_string(chassisNum.value()));
+            }
+            chassisHolderPlace = shortName.find(chassisHolder);
+            if (chassisHolderPlace != std::string::npos)
+            {
+                shortName.replace(chassisHolderPlace, chassisHolder.length(),
+                                  std::to_string(chassisNum.value()));
+            }
+            FanAssignment def{.type = fan["type"].get<std::string>(),
+                              .inventoryBase = inventoryBase,
+                              .shortName = shortName};
+            fanAssigns.push_back(def);
+        }
+    }
+    else
+    {
+        for (const auto& fan : fanList)
+        {
+            if (!fan.contains("type") || !fan.contains("inventory_base") ||
+                !fan.contains("short_name"))
+            {
+                lg2::error(
+                    "Missing one of required fields 'type, inventory_base, short_name' in fan object");
+                throw std::runtime_error(
+                    "Missing required field in fan object");
+            }
+            FanAssignment def{
+                .type = fan["type"].get<std::string>(),
+                .inventoryBase = fan["inventory_base"].get<std::string>(),
+                .shortName = fan["short_name"].get<std::string>()};
+            fanAssigns.push_back(def);
+        }
+    }
+    return fanAssigns;
+}
+
+} // namespace phosphor::fan::monitor::multi_chassis
